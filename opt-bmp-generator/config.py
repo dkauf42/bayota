e6adaab7f835dbca8ad44a2aa42ec0d68f5433c7
dkauf42 <dkauf42@gmail.com>
2018-01-05 10:49:11 -0500

    class docstrings added

--- opt-bmp-generator/config.py
+++ opt-bmp-generator/config.py
@@ -2,6 +2,20 @@ import pandas as pd
 
 
 class ConfigObj:
+    """Loads an 'options' file that represents the user choices for a particular scenario
+
+    Parameters
+    ----------
+    optionsfile : `str`
+        file path of the 'options' csv file for the user scenario
+
+    Notes
+    -----
+    The options file should have the following columns:
+        - BaseCondition,LandRiverSegment,CountyName,StateAbbreviation,StateBasin,OutOfCBWS,AgencyCode
+    Any blank options should be specified by a '-'
+
+    """
     def __init__(self, optionsfile="../options_AAcounty.txt"):
         self.options = None
 
