f78813fa338aa2d0870c5fa68a78e8e8be8ffc83
dkauf42 <dkauf42@gmail.com>
2018-01-09 11:15:16 -0500

    pass over the __main__ check to run in an interactive python shell

--- opt-bmp-generator/main.py
+++ opt-bmp-generator/main.py
@@ -4,15 +4,20 @@
 Test various BMP-load source combinations
 """
 import timeit
+import sys
+import os
+script_dir = os.path.dirname('__file__')  # <-- absolute dir of this script
+sys.path.append(script_dir)
 from util.InputsToCast import InputsToCast
 from util.Scenario import Scenario
 
 #  MAIN FUNCTION  #
 if __name__ == "__main__":
-
+    pass
+else:
     start_time = timeit.default_timer()
     # A suite of Geo objects, specified in the options file, is loaded.
-    Scenario(optionsfile="../test/options_multiple.txt")
+    scenario = Scenario(optionsfile="../test/options_AAcounty.txt")
     print("Loading time", timeit.default_timer() - start_time)
 
     #for g in geosuite.geoobjs:
