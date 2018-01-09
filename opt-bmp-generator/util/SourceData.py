886d854eacbb04995865b99d7a8046bc8f748805
dkauf42 <dkauf42@gmail.com>
2018-01-09 11:13:03 -0500

    add the 'load source conversion BMPs' sheet to be read in.

--- opt-bmp-generator/util/SourceData.py
+++ opt-bmp-generator/util/SourceData.py
@@ -7,15 +7,18 @@ srcdatadir = '/Users/Danny/Desktop/CATEGORIES/CAREER_MANAGEMENT/CRC-ResearchScie
 class SourceData(ExcelDataTable):
     def __init__(self, filename='SourceData_wPossibilities.xlsx', dirpath=srcdatadir):
 
-        ExcelDataTable.__init__(self, filename=filename, dirpath=dirpath,
-                                sheet_names=['Geographic References', 'Efficiency BMPs',
-                                             'Agencies', 'Load Source Definitions'])
+        sheet_names = ['Geographic References', 'Efficiency BMPs',
+                       'Load Source Conversion BMPs', 'Agencies',
+                       'Load Source Definitions']
+
+        ExcelDataTable.__init__(self, filename=filename, dirpath=dirpath, sheet_names=sheet_names)
 
         # Data from excel sheets are saved to class attributes.
-        self.georefs = self.data['Geographic References']
-        self.efficiencyBMPs = self.data['Efficiency BMPs']
-        self.agencies = self.data['Agencies']  # Columns of interest: 'AgencyCode' and 'AgencyType'
-        self.lsdefinitions = self.data['Load Source Definitions']
+        self.georefs = self.data[sheet_names[0]]
+        self.efficiencyBMPs = self.data[sheet_names[1]]
+        self.sourceconversionBMPs = self.data[sheet_names[2]]
+        self.agencies = self.data[sheet_names[3]]  # Columns of interest: 'AgencyCode' and 'AgencyType'
+        self.lsdefinitions = self.data[sheet_names[4]]
 
     def getallnames(self, nametype):
         if nametype == 'LandRiverSegment':
