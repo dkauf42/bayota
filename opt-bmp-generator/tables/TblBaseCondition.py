c44a7f217bfc9c469817e7aebc1587b95177f0ea
dkauf42 <dkauf42@gmail.com>
2018-01-10 16:29:38 -0500

    Updated file paths

--- opt-bmp-generator/tables/TblBaseCondition.py
+++ opt-bmp-generator/tables/TblBaseCondition.py
@@ -1,11 +1,11 @@
-from util.ExcelDataTable import ExcelDataTable
+from tables.ExcelDataTable import ExcelDataTable
 
 reportsdir = '/Users/Danny/Desktop/CATEGORIES/CAREER_MANAGEMENT/CRC-ResearchScientist-Optimization/' \
-              'Optimization_Tool/2-ExperimentFolder/data_tables/CAST_reports/'
+              'Optimization_Tool/2-ExperimentFolder/data_tables/source_and_base/2016progress/'
 
 
 class BaseCondition(ExcelDataTable):
-    def __init__(self, filename='2016Progress_BaseConditions.xlsx', dirpath=reportsdir):
+    def __init__(self, filename='2016ProgressV8-BaseConditions.xlsx', dirpath=reportsdir):
 
         ExcelDataTable.__init__(self, filename=filename, dirpath=dirpath,
                                 sheet_names=['Landuse Acres'])
