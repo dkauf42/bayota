9fdbf78199ea9533586f16d9d7ea92baf7e08b62
dkauf42 <dkauf42@gmail.com>
2018-01-10 14:36:59 -0500

    fixed class name of TblManureTonsProduced

--- opt-bmp-generator/util/TblManureTonsProduced.py
+++ opt-bmp-generator/util/TblManureTonsProduced.py
@@ -4,7 +4,7 @@ reportsdir = '/Users/Danny/Desktop/CATEGORIES/CAREER_MANAGEMENT/CRC-ResearchScie
               'Optimization_Tool/2-ExperimentFolder/data_tables/CAST_reports/'
 
 
-class BaseCondition(ExcelDataTable):
+class ManureTonsProduced(ExcelDataTable):
     def __init__(self, filename='test_forestbuffer_ManureTonsProduced.xlsx', dirpath=reportsdir):
 
         ExcelDataTable.__init__(self, filename=filename, dirpath=dirpath,
