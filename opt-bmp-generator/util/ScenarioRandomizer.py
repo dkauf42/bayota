2cb09b4b122c988dc8a0a1604f91c4d2ee47af8a
dkauf42 <dkauf42@gmail.com>
2018-01-18 12:39:10 -0500

    add printed comment

--- opt-bmp-generator/util/ScenarioRandomizer.py
+++ opt-bmp-generator/util/ScenarioRandomizer.py
@@ -13,6 +13,7 @@ class ScenarioRandomizer:
     def rand_integers(self):
         # Generate a random integer between 1 and 1000 for each non-zero SAS-B combination
         n = len(self.possmatrix.data.index)
+        print('Generating random integers for each SAS-B combination')
         for index, row in tqdm(self.possmatrix.data.iterrows(), total=n):
             numones = (row == 1).sum()
             row[row == 1] = random.sample(range(1, 1000), numones)
