93fff5d9aedf1acab734bb4bf8b1f06225b0a398
dkauf42 <dkauf42@gmail.com>
2018-02-07 17:38:19 -0500

    improve docstrings

--- OptSandbox/tables/ExcelDataTable.py
+++ OptSandbox/tables/ExcelDataTable.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 class ExcelDataTable:
     def __init__(self, filename='', dirpath='', sheet_names=None):
+        """A parent class for various data tables that are excel formatted"""
 
         self.filename = filename
         self.fullpath = dirpath + self.filename
@@ -18,17 +19,21 @@ class ExcelDataTable:
         self.data = pd.read_excel(self.fullpath, sheet_name=self.sheet_names)
 
     def get(self, sheetabbrev='georefs', getcolumn='LandRiverSegment', by='CountyName', equalto='Anne Arundel'):
-        """
-        Get the values from rows that match criteria in another data column
-        :param sheetabbrev: Excel Sheet Abbreviation; For example: georefs, efficiencyBMPs, deliveryfactors, etc.
-        :param getcolumn: For example: LandRiverSegment, LoadSource, etc.
-        :param by: For example: CountyName, LandRiverSegment, etc.
-        :param equalto: For example: Anne Arundel, MD, N24003WL0_4390_0000, etc.
-        :return: subset dataframe
+        """Get the values from rows that match criteria in another data column
 
-        For example:
-        sheetabbrev='georefs', getcolumn='LandRiverSegment', by='CountyName', equalto='Anne Arundel'
-        sheetabbrev='LSacres', getcolumn='PreBMPAcres', by='LoadSource', equalto='Ag Open Space'
+        Parameters:
+            sheetabbrev (str): Excel Sheet Abbreviation; For example: georefs, efficiencyBMPs, deliveryfactors, etc.
+            getcolumn (str): For example: LandRiverSegment, LoadSource, etc.
+            by (str): For example: CountyName, LandRiverSegment, etc.
+            equalto (str): For example: Anne Arundel, MD, N24003WL0_4390_0000, etc.
+
+        Returns:
+            (pandas dataframe): a subset of the original table
+
+        Example:
+            sheetabbrev='georefs', getcolumn='LandRiverSegment', by='CountyName', equalto='Anne Arundel'
+            sheetabbrev='LSacres', getcolumn='PreBMPAcres', by='LoadSource', equalto='Ag Open Space'
+
         """
         df = self[sheetabbrev]
 
