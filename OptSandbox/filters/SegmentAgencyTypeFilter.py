3fe897da6b0a22e846c4702f6929ed1f76ae637b
dkauf42 <dkauf42@gmail.com>
2018-02-07 17:32:57 -0500

    improve the docstrings

--- OptSandbox/filters/SegmentAgencyTypeFilter.py
+++ OptSandbox/filters/SegmentAgencyTypeFilter.py
@@ -3,13 +3,25 @@ import pandas as pd
 
 class SegmentAgencyTypeFilter:
     def __init__(self, tables=None, includespec=None):
-        """Find the segment - agency - source combinations available in the specified options."""
-
         """Find the load sources (along with their maxes {acres, miles, units, etc.}) in the specified SATs
-                    option headers = BaseCondition, LandRiverSegment, CountyName, StateAbbreviation, StateBasin,
-                                     OutOfCBWS, AgencyCode, Sector
+
+        Parameters:
+            tables (obj):
+            includespec (obj)
+
+        Attributes:
+            lsndas (pandas dataframe)
+            lsani (pandas dataframe)
+            lsman (pandas dataframe)
+
+        Note:
+            option headers =
+                [BaseCondition, LandRiverSegment, CountyName, StateAbbreviation,
+                StateBasin, OutOfCBWS, AgencyCode, Sector]
+
         """
-        # Loop through tables (lsnatural, lsdeveloped, lsagriculture, lsmanure, lsanimal (basecond for now), lsmanure)
+        # Loop through tables
+        # (lsnatural, lsdeveloped, lsagriculture, lsmanure, lsanimal (basecond for now), lsmanure)
         self._lsnat = self._filter_from_options(includespec, tables,
                                                 prebmpls_df=tables.lsnatural.PreBmpLoadSourceNatural)
         self._lsdev = self._filter_from_options(includespec, tables,
@@ -28,8 +40,11 @@ class SegmentAgencyTypeFilter:
     def _filter_animals_from_options(self, includespec, prebmpls_df):
         """Find the load sources in the specified SATs within a prebmpls_df
 
-            option headers = BaseCondition, LandRiverSegment, CountyName, StateAbbreviation, StateBasin,
-                             OutOfCBWS, AgencyCode, Sector
+        Note:
+            option headers = BaseCondition, LandRiverSegment, CountyName,
+                StateAbbreviation, StateBasin,
+                OutOfCBWS, AgencyCode, Sector
+
         """
         # For Animals and Manure, we only need to filter by County (geographically) and not any agencies
         lsdf_geobool = pd.DataFrame()
@@ -46,8 +61,10 @@ class SegmentAgencyTypeFilter:
     def _filter_from_options(self, includespec, tables, prebmpls_df):
         """Find the load sources in the specified SATs within a prebmpls_df
 
-            option headers = BaseCondition, LandRiverSegment, CountyName, StateAbbreviation, StateBasin,
-                             OutOfCBWS, AgencyCode, Sector
+        Note:
+            option headers = BaseCondition, LandRiverSegment, CountyName,
+                StateAbbreviation, StateBasin,
+                OutOfCBWS, AgencyCode, Sector
         """
 
         # First figure out the filtering by Geography
@@ -79,9 +96,10 @@ class SegmentAgencyTypeFilter:
 
     @staticmethod
     def containssimilarcol(dataframe, name):
-        """Returns
-        (1) True/False if the name is represented in one of the column headers of the dataframe
-        (2) <list str> name that is represented as a column header of the dataframe
+        """
+        Returns:
+            (1) True/False if the name is represented in one of the column headers of the dataframe
+            (2) (list str) name that is represented as a column header of the dataframe
         """
         agencyheader = ['Agency', 'AgencyCode']
         if name in agencyheader:
