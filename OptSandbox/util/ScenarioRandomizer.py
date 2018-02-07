074091265bc047df27285ed524399d3a85f683b3
dkauf42 <dkauf42@gmail.com>
2018-02-07 17:45:48 -0500

    improve docstring

--- OptSandbox/util/ScenarioRandomizer.py
+++ OptSandbox/util/ScenarioRandomizer.py
@@ -3,10 +3,15 @@ import random
 
 class ScenarioRandomizer:
     def __init__(self, dataframe=None):
-        """Generate random integers for each non-empty (Geo, Agency, Source, BMP) coordinate"""
+        """Generate random integers for each non-empty (Geo, Agency, Source, BMP) coordinate
+
+        Parameters:
+            dataframe (pandas dataframe):
+
+        """
         self._rand_integers(dataframe)
 
     @staticmethod
     def _rand_integers(dataframe):
         howmanytoreplace = (dataframe == 1).sum().sum()
-        dataframe[dataframe == 1] = random.sample(range(1, 200000), howmanytoreplace)
+        dataframe[dataframe == 1] = random.sample(range(1, howmanytoreplace+1), howmanytoreplace)
