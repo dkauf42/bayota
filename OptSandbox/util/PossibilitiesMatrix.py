631366301967367ede11fa1bffff2aa45a408a48
dkauf42 <dkauf42@gmail.com>
2018-02-07 17:43:57 -0500

    add the FIPSFrom and FIPSTo fields

--- OptSandbox/util/PossibilitiesMatrix.py
+++ OptSandbox/util/PossibilitiesMatrix.py
@@ -1,6 +1,8 @@
 from scipy import sparse
 import pandas as pd
+import numpy as np
 from tqdm import tqdm  # Loop progress indicator module
+from itertools import product
 
 from filters.SegmentAgencyTypeFilter import SegmentAgencyTypeFilter
 
@@ -15,12 +17,35 @@ def _create_emptydf(row_indices, column_names):
     return df
 
 
+def expand_grid(data_dict):
+    rows = product(*data_dict.values())
+    return pd.DataFrame.from_records(rows, columns=data_dict.keys())
+
+
 class PossibilitiesMatrix:
     def __init__(self, tables=None, includespec=None):
-        """Filter by Segment-agency_types"""
+        """Filter by Segment-agency_types
 
-        # Options are used to query the BaseCondition data and get the Load Sources for each segment-agency pair
+        Parameters:
+            tables (obj)
+            includespec (obj)
+
+
+        Attributes:
+            ndas (pandas dataframe)
+            anim (pandas dataframe)
+            manu (pandas dataframe)
+            bmpdict (dictionary)
+            geo_seg_source_bmps (pandas dataframe): agencies to be included in this Scenario
+
+        """
+
+        # Options are used to query BaseCondition data and
+        # get the LoadSources (along with their maxes) for each segment-agency pair
         sat = SegmentAgencyTypeFilter(tables=tables, includespec=includespec)
+
+        #  A series of possible FIPSFrom and FIPSTo combinations is generated
+        fips_pairs = self._create_possibleFIPSpairs(includespec=includespec)
 
         # A sparse matrix is created for each Segment-Agency-Type table.
         # For the Land table,   the specs are rows=seg-agency-sources X columns=BMPs.
@@ -29,19 +54,20 @@ class PossibilitiesMatrix:
         self.ndas = None
         self.anim = None
         self.manu = None
-        self._create_emptymatrices(sat, tables)
+        self._create_emptymatrices(sat, tables, fips_pairs)
 
-        #upper_bounds = self.__identifyhardupperbounds(sat)
+        #  TODO: upper_bounds = self._identifyhardupperbounds(sat)
 
         # Get a series of all the LoadSources in this scenario
         allloadsources = pd.concat([self.ndas.index.get_level_values('LoadSource').to_series(),
                                     self.anim.index.get_level_values('LoadSource').to_series(),
                                     self.manu.index.get_level_values('LoadSource').to_series()],
                                    ignore_index=True).unique()
+
         # A dictionary is generated with <keys:loadsource>, <values:eligible BMPs>.
         self.bmpdict = self._dict_of_bmps_by_loadsource(tables.srcdata, allloadsources)
 
-        # Nonempty markers are generated for eligible (Geo, Agency, Source, BMP) coordinates in the possibilities matrix
+        # NonNaN markers are generated for eligible (Geo, Agency, Source, BMP) coordinates in the possibilities matrix
         self.geo_seg_source_bmps = None
         self.filter_from_sat(dataframe=self.ndas, srcdataobj=tables.srcdata)
         self.filter_from_sat(dataframe=self.anim, srcdataobj=tables.srcdata)
@@ -51,14 +77,48 @@ class PossibilitiesMatrix:
         self.anim.to_csv('./output/testwrite_PossibilitiesMatrix_anim.csv')
         self.manu.to_csv('./output/testwrite_PossibilitiesMatrix_manu.csv')
 
-    def _create_emptymatrices(self, sat, tables):
+    def _create_possibleFIPSpairs(self, includespec):
+        """ A series of possible FIPSFrom and FIPSTo combinations is generated """
+
+        fips_list = includespec.geo.FIPS.unique().tolist()
+        fips_list.append(98765)
+        #fips_list.append(12345)
+        fips_permutation_pairs = [p for p in product(fips_list, repeat=2)]
+
+        newdf = pd.DataFrame()
+        for n, col in enumerate(['FIPSFrom', 'FIPSTo']):
+            newdf[col] = [i[n] for i in fips_permutation_pairs]
+        return newdf
+
+    def _create_emptymatrices(self, sat, tables, fips_pairs):
         # Create a sparse matrix for each sat table with rows=seg-agency-sources X columns=BMPs
         lsndas_indexed = sat.lsndas.set_index(['LandRiverSegment', 'Agency', 'LoadSource']).copy()
         self.ndas = _create_emptydf(row_indices=lsndas_indexed.index, column_names=tables.srcdata.allbmps_shortnames)
         lsani_indexed = sat.lsani.set_index(['FIPS', 'AnimalName', 'LoadSource']).copy()
         self.anim = _create_emptydf(row_indices=lsani_indexed.index, column_names=tables.srcdata.allbmps_shortnames)
+
+        #  lsman_indexed.rename(columns={"FIPS": "FIPSFrom"}, inplace=True)
         lsman_indexed = sat.lsman.set_index(['FIPS', 'AnimalName', 'LoadSource']).copy()
-        self.manu = _create_emptydf(row_indices=lsman_indexed.index, column_names=tables.srcdata.allbmps_shortnames)
+        #print(fips_pairs['FIPSTo'])
+        #lsman_indexed['FIPSTo'] = np.nan  # First, add FIPSTo as a normal column
+        #lsman_indexed.set_index('FIPSTo', append=True, inplace=True)  # then append FIPSTo to the current index
+
+        #  All the possible FIPSFrom and FIPSTo combinations are generated.
+        newdf = expand_grid({'FIPSFrom': sat.lsman.FIPS.unique(),
+                             'FIPSTo': sat.lsman.FIPS.unique(),
+                             'AnimalName': sat.lsman.AnimalName.unique(),
+                             'LoadSource': sat.lsman.LoadSource.unique()})
+        newdf_indexed = newdf.set_index(['FIPSFrom', 'FIPSTo', 'AnimalName', 'LoadSource']).copy()
+        newdf_indexed['Amount'] = np.nan  # add Amount as a normal column
+
+        # print(newdf[['FIPSFrom', 'FIPSTo']])
+        #   newdf_indexed.to_csv('testwrite_expandgrid_FIPS.csv')
+        # print(lsman_indexed.index.levels)
+        #lsman_indexed.index.rename(['FIPSFrom', 'AnimalName', 'LoadSource', 'FIPSTo'])
+        #print(newdf_indexed.head())
+        #print(newdf_indexed.index.levels)
+        self.manu = _create_emptydf(row_indices=newdf_indexed.index, column_names=tables.srcdata.allbmps_shortnames)
+        # print(self.manu.head())
 
     def _dict_of_bmps_by_loadsource(self, srcdataobj, load_sources):
         """ Generate a dictionary of BMPs that are eligible for every load source """
@@ -80,7 +140,7 @@ class PossibilitiesMatrix:
         return ls_to_bmp_dict
 
     def filter_from_sat(self, dataframe, srcdataobj):
-        """Find the segment - agency - source combinations available in the specified options.
+        """Generate nonNaN markers for eligible (Geo, Agency, Source, BMP) coordinates in the possibilities matrix
         """
         # Get all the BMPs that are possible on the set of Load sources
         self.geo_seg_source_bmps = dataframe.copy()
@@ -93,7 +153,7 @@ class PossibilitiesMatrix:
         loadsourceindex = dataframe.index.names.index('LoadSource')
         for index, row in tqdm(dataframe.iterrows(), total=n):  # iterate through the load sources
 
-            # Mark the eligible BMPs for each Load Source with a 999 instead of a NaN
+            # Mark the eligible BMPs for each Load Source with a '1' instead of a NaN
             bmplist = self.bmpdict[row.name[loadsourceindex]]
             #print(bmplist)
             #dataframe.loc[(index[0], index[1], row.LoadSource), bmplist] = 1
