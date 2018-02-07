92d7d379dac81e06c70901e5db16c7cced634930
dkauf42 <dkauf42@gmail.com>
2018-02-07 17:55:41 -0500

    remove old and unused text from main.py

--- OptSandbox/main.py
+++ OptSandbox/main.py
@@ -15,19 +15,10 @@ sys.path.append(script_dir)
 
 def main():
     start_time = timeit.default_timer()
-    # A suite of Geo objects, specified in the options file, is loaded.
-    scenario = Scenario(optionsfile="../test/options_AAcounty.txt")
+    # A scenario is generated from options, and possibility matrices are generated.
+    Scenario(optionsfile="../test/options_AAcounty.txt")
     print("Loading time", timeit.default_timer() - start_time)
-    #for g in geosuite.geoobjs:
-    #    print(g)
-
-    raise ValueError('<DONE>')
-    print(AACounty.land_river_segments.iloc[0])
-    LSs = AACounty.loadsources_by_lrs(AACounty.land_river_segments.iloc[0])
-    print(LSs)
-    print(AACounty.countloadsources())
-    castinput = CastInputObj()
-    #castinput.create_landbmp_file()
+    print('<DONE>')
 
 
 if __name__ == '__main__':
