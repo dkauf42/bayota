5d2a22ef21b3a8c80209b3909bde52c39c99e74e
dkauf42 <dkauf42@gmail.com>
2018-02-20 17:20:07 -0500

    add buttons for adding all or removing all of the selected items

--- OptSandbox/gui/useframes/dualbox.py
+++ OptSandbox/gui/useframes/dualbox.py
@@ -14,6 +14,7 @@ class DualBox(tk.Frame):
         if len(list_items) > len(set(list_items)):
             warnings.warn("DualBox: duplicates are present in list. Removing duplicates...")
             list_items = list(OrderedDict.fromkeys(list_items))  # remove duplicates
+        list_items = sorted(list_items, key=str.lower)  # list is alphabetized
 
         # Left-hand side Listbox
         self.listBox = Pmw.ScrolledListBox(self, items=list_items,
@@ -26,15 +27,18 @@ class DualBox(tk.Frame):
         # Buttons
         self.buttonframe = tk.Frame(self)
         self.buttonframe.pack(side=tk.LEFT, expand=tk.YES, fill=tk.BOTH, padx=5, pady=5)
+        # Copy All Button
+        self.removeButton = tk.Button(self.buttonframe, text=">>>", command=self.add_all_items_to_chosen)
+        self.removeButton.grid(row=0, column=0, sticky='we')
         # Copy Button
-        self.copyButton = tk.Button(self.buttonframe, text=">>>", command=self.add_item_to_chosen)
-        self.copyButton.grid(row=0, column=0, sticky='we')
+        self.copyButton = tk.Button(self.buttonframe, text=">", command=self.add_item_to_chosen)
+        self.copyButton.grid(row=1, column=0, sticky='we')
         # Remove Button
-        self.removeButton = tk.Button(self.buttonframe, text="<<<", command=self.remove_item_from_chosen)
-        self.removeButton.grid(row=1, column=0, sticky='we')
-        # Clear Button
-        self.clearButton = tk.Button(self.buttonframe, text="clear", command=self.clear_all_chosen_items)
-        self.clearButton.grid(row=2, column=0, sticky='we')
+        self.removeButton = tk.Button(self.buttonframe, text="<", command=self.remove_item_from_chosen)
+        self.removeButton.grid(row=2, column=0, sticky='we')
+        # Remove All Button
+        self.removeButton = tk.Button(self.buttonframe, text="<<<", command=self.remove_all_items_from_chosen)
+        self.removeButton.grid(row=3, column=0, sticky='we')
 
         # Right-hand side Listbox
         self.chosen = Pmw.ScrolledListBox(self, items='',
@@ -47,12 +51,19 @@ class DualBox(tk.Frame):
     def set_new_left_side_items(self, items):
         if not isinstance(items, list):
             raise TypeError('"items" argument must be a <list>')
-        self.clear_all_chosen_items()
+        self.remove_all_items_from_chosen()
         list_items = sorted(items, key=str.lower)  # list is alphabetized
         self.listBox.setlist(list_items)
 
     def get_selection(self):
         return list(self.chosen.get(0, tk.END))
+
+    def add_all_items_to_chosen(self):
+        original_chosen_items = self.chosen.get(0, tk.END)
+        all_items_to_add = self.listBox.get()
+        self.chosen.setlist(original_chosen_items + all_items_to_add)
+        # ..and remove from left-side box
+        self.listBox.clear()
 
     def add_item_to_chosen(self):
         original_chosen_items = self.chosen.get(0, tk.END)
@@ -88,7 +99,7 @@ class DualBox(tk.Frame):
             list_items = sorted(original_unselected_items + tuple(selected_to_remove), key=str.lower)
             self.listBox.setlist(list_items)
 
-    def clear_all_chosen_items(self):
+    def remove_all_items_from_chosen(self):
         original_unselected_items = self.listBox.get()
         original_chosen_items = self.chosen.get(0, tk.END)
         self.chosen.clear()
