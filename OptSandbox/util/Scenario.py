9dc25ba6aadb296ae0353337bdde81e77888d467
dkauf42 <dkauf42@gmail.com>
2018-02-26 10:17:51 -0500

    Include new class in the scenario runner

--- OptSandbox/util/Scenario.py
+++ OptSandbox/util/Scenario.py
@@ -4,6 +4,8 @@ from filters.IncludeSpec import IncludeSpec
 from util.PossibilitiesMatrix import PossibilitiesMatrix
 from util.ScenarioRandomizer import ScenarioRandomizer
 from util.InputsToCast import InputsToCast
+from util.OptInstance import OptInstance
+from tables.TblQueries import TblQuery
 
 import tkinter as tk
 from gui.toplevelframes.mainwindow import MainWindow
@@ -22,31 +24,33 @@ class Scenario:
 
         """
         # Load the Source Data and Base Condition tables
-        self.tables = TblLoader()
+        tablesobj = TblLoader()
+        tblqueryobj = TblQuery(tablesobj)
+        oinstance = OptInstance()
 
         """ TO RUN WITH A GUI """
-        """
+        #"""
         # Create a tkinter window for the mainwindow
         self.root = tk.Tk()
         # Start the GUI
-        self.run_gui()
+        self.run_gui(tblquery=tblqueryobj, optinstance=oinstance)
         self.root.mainloop()
         if not hasattr(self.root, 'results'):
             raise ValueError('No options specified.')
         else:
             print(self.root.results)
         raise ValueError('Scenario: temp halt')
-        """
+        #"""
 
         # The scenario options (particular geographic region(s), agencies, etc.) are loaded for this scenario.
-        self.options = OptionLoader(srcdataobj=self.tables.srcdata)
+        self.options = OptionLoader(srcdataobj=tablesobj.srcdata)
         self.options.load_from_csv(optionsfile=optionsfile)
 
         # Geographic region is extracted from SourceData, so the list of lrsegs (etc.) can be used as a boolean mask
-        self.includespec = IncludeSpec(optionloaderobj=self.options, tables=self.tables)
+        self.includespec = IncludeSpec(optionloaderobj=self.options, tables=tablesobj)
 
         # Generate a matrix with rows(i)=seg-agency-sources X columns(j)=BMPs
-        self.possmatrix = PossibilitiesMatrix(tables=self.tables, includespec=self.includespec)
+        self.possmatrix = PossibilitiesMatrix(tables=tablesobj, includespec=self.includespec)
 
         # Populate the Possibilities Matrix with a random assortment of numbers for each ST-B combination
         print('>> Generating random integers for each (Geo, Agency, Source, BMP) coordinate')
@@ -58,13 +62,13 @@ class Scenario:
         self.possmatrix.anim.to_csv('./output/testwrite_Scenario_possmatrix_anim.csv')  # write possibilities matrix to file
         self.possmatrix.manu.to_csv('./output/testwrite_Scenario_possmatrix_manu.csv')  # write possibilities matrix to file
 
-        inputobj = InputsToCast(self.possmatrix, tables=self.tables)
+        inputobj = InputsToCast(self.possmatrix, tables=tablesobj)
         inputobj.matrix_to_table()
 
         print('<Scenario Loading Complete>')
 
-    def run_gui(self):
-        with MainWindow(self.root) as mainwindow:
+    def run_gui(self, tblquery=None, optinstance=None):
+        with MainWindow(self.root, tblquery, optinstance) as mainwindow:
             mainwindow.pack(side="top", fill="both", expand=True)
             self.root.title("Optimization Options")
 
