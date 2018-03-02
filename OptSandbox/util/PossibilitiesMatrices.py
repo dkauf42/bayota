abb29e89fb0735721c87c68819fa58937e60a11d
dkauf42 <dkauf42@gmail.com>
2018-03-02 17:37:56 -0500

    Move code out of init that doesn't need to be there

--- OptSandbox/util/PossibilitiesMatrices.py
+++ OptSandbox/util/PossibilitiesMatrices.py
@@ -10,62 +10,45 @@ class PossibilitiesMatrices:
         """Filter by Segment-agency_types
 
         Args:
-            optinstance (obj)
+            optinstance (obj): This is the optimization instance
 
         Attributes:
-            ndas (pandas.DataFrame):
+            ndas (obj):
+            anim (obj):
+            manu (obj):
+
         """
+        # Determine what will comprise the rows (indices of load source names) of the possibility matrix
         loadsources = optinstance.queries.loadsources.\
             get_load_sources_in_geoagencies(geographies=optinstance.geographies_included,
                                             agencies=optinstance.agencies_included)
-
-        # A sparse matrix is created for each Segment-Agency-Type table.
-        # For the Land table,   the specs are rows=seg-agency-sources X columns=BMPs.
-        # For the Animal table, the specs are rows=FIPS-animal-sources X columns=BMPs.
-        # For the Manure table, the specs are rows=FIPSto-FIPSfrom-animal-sources X columns=BMPs.
-        # Create a sparse matrix for each load source table with rows=seg-agency-sources X columns=BMPs
         lsndas_indexed = loadsources.ndas.set_index(['LandRiverSegment', 'Agency', 'LoadSource']).copy()
         lsani_indexed = loadsources.animal.set_index(['FIPS', 'AnimalName', 'LoadSource']).copy()
-        #ndas.create_emptydf(row_indices=lsndas_indexed.index,
-        #                    column_names=optinstance.tables.srcdata.allbmps_shortnames)
-        #anim.create_emptydf(row_indices=lsani_indexed.index,
-        #                    column_names=optinstance.tables.srcdata.allbmps_shortnames)
+        #  For manure, all the possible FIPSFrom and FIPSTo combinations are generated.
+        newdf_manure = MatrixBase.expand_grid({'FIPSFrom': loadsources.manure.FIPS.unique(),
+                                        'FIPSTo': loadsources.manure.FIPS.unique(),
+                                        'AnimalName': loadsources.manure.AnimalName.unique(),
+                                        'LoadSource': loadsources.manure.LoadSource.unique()})
+        newdf_manure_indexed = newdf_manure.set_index(['FIPSFrom', 'FIPSTo', 'AnimalName', 'LoadSource']).copy()
+        newdf_manure_indexed['Amount'] = np.nan  # add Amount as a normal column
 
+        """ A sparse matrix is created for each Segment-Agency-Type table.
+        The Specs:
+            Land: rows=seg-agency-sources X columns=BMPs.
+            Animal: rows=FIPS-animal-sources X columns=BMPs
+            Manure: rows=FIPSto-FIPSfrom-animal-sources X columns=BMPs
+        """
         self.ndas = MatrixBase(name='ndas', row_indices=lsndas_indexed.index,
                                column_names=optinstance.tables.srcdata.allbmps_shortnames)
         self.anim = MatrixBase(name='anim', row_indices=lsani_indexed.index,
                                column_names=optinstance.tables.srcdata.allbmps_shortnames)
-
-        #  For manure, all the possible FIPSFrom and FIPSTo combinations are generated.
-        newdf = MatrixBase.expand_grid({'FIPSFrom': loadsources.manure.FIPS.unique(),
-                                        'FIPSTo': loadsources.manure.FIPS.unique(),
-                                        'AnimalName': loadsources.manure.AnimalName.unique(),
-                                        'LoadSource': loadsources.manure.LoadSource.unique()})
-        newdf_indexed = newdf.set_index(['FIPSFrom', 'FIPSTo', 'AnimalName', 'LoadSource']).copy()
-        newdf_indexed['Amount'] = np.nan  # add Amount as a normal column
-
-        self.manu = MatrixBase(name='manu', row_indices=newdf_indexed.index,
+        self.manu = MatrixBase(name='manu', row_indices=newdf_manure_indexed.index,
                                column_names=optinstance.tables.srcdata.allbmps_shortnames)
 
-        # A sparse matrix is created for each Segment-Agency-Type table.
-        # For the Land table,   the specs are rows=seg-agency-sources X columns=BMPs.
-        # For the Animal table, the specs are rows=FIPS-animal-sources X columns=BMPs.
-        # For the Manure table, the specs are rows=FIPSto-FIPSfrom-animal-sources X columns=BMPs.
-        #self._create_emptymatrices(optinstance=optinstance, ndas=ndas, anim=anim, manu=manu)
-
-        # Get a series of all the LoadSources in this scenario
-        allloadsources = pd.concat([self.ndas.matrix.index.get_level_values('LoadSource').to_series(),
-                                    self.anim.matrix.index.get_level_values('LoadSource').to_series(),
-                                    self.manu.matrix.index.get_level_values('LoadSource').to_series()],
-                                   ignore_index=True).unique()
-
-        # A dictionary is generated with <keys:loadsource>, <values:eligible BMPs>.
-        self.bmpdict = self._dict_of_bmps_by_loadsource(optinstance.tables.srcdata, allloadsources)
-
         # NonNaN markers are generated for eligible (Geo, Agency, Source, BMP) coordinates in the possibilities matrix
-        self.mark_eligible_coordinates(dataframe=self.ndas.matrix, srcdataobj=optinstance.tables.srcdata)
-        self.mark_eligible_coordinates(dataframe=self.anim.matrix, srcdataobj=optinstance.tables.srcdata)
-        self.mark_eligible_coordinates(dataframe=self.manu.matrix, srcdataobj=optinstance.tables.srcdata)
+        self.mark_eligible_coordinates(dataframe=self.ndas.matrix, optinstance=optinstance)
+        self.mark_eligible_coordinates(dataframe=self.anim.matrix, optinstance=optinstance)
+        self.mark_eligible_coordinates(dataframe=self.manu.matrix, optinstance=optinstance)
 
         #  TODO: upper_bounds = self._identifyhardupperbounds(sat)
         # Associate a hard lower and upper bound with each marker coordinate in the matrix
@@ -74,13 +57,16 @@ class PossibilitiesMatrices:
         self.anim.matrix.to_csv('./output/testwrite_PossibilitiesMatrix_anim.csv')
         self.manu.matrix.to_csv('./output/testwrite_PossibilitiesMatrix_manu.csv')
 
-    def mark_eligible_coordinates(self, dataframe=None, srcdataobj=None):
+    def mark_eligible_coordinates(self, dataframe=None, optinstance=None):
         """Generate nonNaN markers for eligible (Geo, Agency, Source, BMP) coordinates in the possibilities matrix
 
         Args:
             dataframe (pandas.DataFrame):
-            srcdataobj (pandas.DataFrame):
+            optinstance (obj):
         """
+        # A dictionary is generated with <keys:loadsource>, <values:eligible BMPs>.
+        bmpdict = self._dict_of_bmps_by_loadsource(srcdataobj=optinstance.tables.srcdata)
+
         # Get all the BMPs that are possible on the set of Load sources
         geo_seg_source_bmps = dataframe.copy()
         bmplistoflists = []  # Create a list to store the data
@@ -91,11 +77,8 @@ class PossibilitiesMatrices:
               'coordinates in the possibilities matrix')
         loadsourceindex = dataframe.index.names.index('LoadSource')
         for index, row in tqdm(dataframe.iterrows(), total=n):  # iterate through the load sources
-
             # Mark the eligible BMPs for each Load Source with a '1' instead of a NaN
-            bmplist = self.bmpdict[row.name[loadsourceindex]]
-            #print(bmplist)
-            #dataframe.loc[(index[0], index[1], row.LoadSource), bmplist] = 1
+            bmplist = bmpdict[row.name[loadsourceindex]]
             row.loc[bmplist] = 1
 
             bmplistoflists.append(bmplist)
@@ -103,7 +86,6 @@ class PossibilitiesMatrices:
             overallbmplist += bmplist
 
         overallbmplist = self.removedups(overallbmplist)
-        overallbmptypes = srcdataobj.findbmptype(overallbmplist)
         print('length of overall bmp list: %d' % len(overallbmplist))
         print(overallbmplist)
 
@@ -120,8 +102,6 @@ class PossibilitiesMatrices:
         geo_seg_source_bmps['eligible_bmps'] = bmplistoflists
         print('total no. of eligible BMPs: <%d>' % totalnumbmps)
 
-        #print(self.geo_seg_source_bmps.head())
-        #print(bmplist[0])
         geo_seg_source_bmps.to_csv('./output/testwrite_PossibilitiesMatrix_geosegsource_bmps.csv')
 
     @staticmethod
@@ -133,10 +113,16 @@ class PossibilitiesMatrices:
                 final_list.append(num)
         return final_list
 
-    def _dict_of_bmps_by_loadsource(self, srcdataobj, load_sources):
+    def _dict_of_bmps_by_loadsource(self, srcdataobj=None):
         """ Generate a dictionary of BMPs that are eligible for every load source """
+
+        # Get a series of all the LoadSources in this scenario
+        all_load_sources = pd.concat([self.ndas.matrix.index.get_level_values('LoadSource').to_series(),
+                                      self.anim.matrix.index.get_level_values('LoadSource').to_series(),
+                                      self.manu.matrix.index.get_level_values('LoadSource').to_series()],
+                                     ignore_index=True).unique()
         ls_to_bmp_dict = {}
-        for ls in load_sources:
+        for ls in all_load_sources:
             # Get the Load Source groups that this Load source is in.
             loadsourcegroups = srcdataobj.get(sheetabbrev='sourcegrpcomponents', getcolumn='LoadSourceGroup',
                                               by='LoadSource', equalto=ls)  # pandas.core.series.Series
