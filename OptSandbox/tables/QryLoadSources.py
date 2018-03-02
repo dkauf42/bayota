81fcdbde6f09844368296761f31eeb066936ebd1
dkauf42 <dkauf42@gmail.com>
2018-03-02 13:02:45 -0500

    Update QryLoadSources class so it reproduces the tables of SATFilter

--- OptSandbox/tables/QryLoadSources.py
+++ OptSandbox/tables/QryLoadSources.py
@@ -11,13 +11,18 @@ class QryLoadSources:
         """
         self.tables = tables
 
-    def get_sources_in_lrsegs(self, lrsegs=None, agencies=None, counties=None, name=''):  # TODO replace the SATFilter._filter_from_options() method with this method
+    def get_sources_in_lrsegs(self, lrsegs=None, agencies=None, counties=None, name=''):
         """Get the load sources present (whether zero acres or not) in the specified segment-agencies"""
         lsdf_geobool = pd.DataFrame()
         lsdf_nongeobool = pd.DataFrame()
 
         # Find the load sources
         if (name == 'natural') | (name == 'developed') | (name == 'agriculture') | (name == 'septic'):
+            print('\nFinding load sources for <%s> in specified LRseg/agencies' % name)
+            if (lrsegs is None) | (agencies is None):
+                raise ValueError('Either "lrsegs" or "agencies" is None, '
+                                 'but must be specified for "%s" load sources' % name)
+
             if name == 'natural':
                 prebmpls_df = self.tables.lsnatural.PreBmpLoadSourceNatural
             elif name == 'developed':
@@ -27,41 +32,40 @@ class QryLoadSources:
             elif name == 'septic':
                 prebmpls_df = self.tables.lsseptic.SepticSystems
             else:
-                raise ValueError('unrecognized source type name: <%s>' % name)
+                raise ValueError('unrecognized source type name: "%s"' % name)
 
-            print('\nFinding load sources for <%s> in specified LRseg/agencies' % name)
-
-            lsdf_geobool['LandRiverSegment'] = prebmpls_df['LandRiverSegment'].isin(lrsegs)
+            lsdf_geobool['LandRiverSegment'] = prebmpls_df['LandRiverSegment'].isin(lrsegs.unique())
             lsdf_geobool = lsdf_geobool.any(axis=1)
-
             lsdf_nongeobool['Agency'] = prebmpls_df['Agency'].isin(agencies)
             lsdf_nongeobool = lsdf_nongeobool.any(axis=1)
 
             lsdf_bool = lsdf_geobool & lsdf_nongeobool
-            print('lsdf_geobool is %d entries long with %d nonzeros' % (len(lsdf_geobool), lsdf_geobool.sum()))
             print('lsdf_nongeobool is %d entries long with %d nonzeros' % (len(lsdf_nongeobool), lsdf_nongeobool.sum()))
             print('lsdf_bool is %d entries long with %d nonzeros' % (len(lsdf_bool), lsdf_bool.sum()))
             filtered_lsdf = prebmpls_df.loc[lsdf_bool, :]
 
         elif (name == 'animal') | (name == 'manure'):
+            print('\nFinding load sources for <%s> in specified County' % name)
+            if counties is None:
+                raise ValueError('"county" is None, but must be specified for "%s" load sources' % name)
+
             if name == 'animal':
                 prebmpls_df = self.tables.basecond.animalcounts
+                countyheader = 'CountyName'
             elif name == 'manure':
                 prebmpls_df = self.tables.lsmanure.ManureTonsProduced
+                countyheader = 'County'
             else:
-                raise ValueError('unrecognized source type name: <%s>' % name)
+                raise ValueError('unrecognized source type name: "%s"' % name)
 
-            print('\nFinding load sources for <%s> in specified County' % name)
-
-            # For Animals and Manure, we only need to filter by County (geographically) and not any agencies
-            lsdf_geobool['CountyName'] = self.getsimilarcol(prebmpls_df, 'CountyName').isin(counties)
+            lsdf_geobool['CountyName'] = prebmpls_df[countyheader].isin(counties.unique())
             lsdf_geobool = lsdf_geobool.any(axis=1)
-            # Filtering by Agency is not needed for Animals and Manure?!
-            print('lsdf_geobool is %d entries long with %d nonzeros' % (len(lsdf_geobool), lsdf_geobool.sum()))
             filtered_lsdf = prebmpls_df.loc[lsdf_geobool, :]
-        else:
-            raise ValueError('unrecognized source type name: <%s>' % name)
 
+        else:
+            raise ValueError('unrecognized source type name: "%s"' % name)
+
+        print('lsdf_geobool is %d entries long with %d nonzeros' % (len(lsdf_geobool), lsdf_geobool.sum()))
         return filtered_lsdf
 
     @staticmethod
