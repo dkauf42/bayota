dad37872683da2d3ae90449f1798deb1ff12186b
dkauf42 <dkauf42@gmail.com>
2018-03-05 16:11:30 -0500

    reword comment string

--- OptSandbox/tables/MatrixAnimal.py
+++ OptSandbox/tables/MatrixAnimal.py
@@ -5,7 +5,7 @@ class MatrixAnimal(MatrixBase):
     def __init__(self, name='', geographies=None, queries=None):
         """Sub-class for Animal BMPs"""
 
-        # Load source with agencies, geo, etc. ("Ya'ad") table is generated.
+        # "Ya'ad" (load sources, agencies, geo, units, amounts, etc.) table is generated.
         self.yaad_table = queries.loadsources.get_yaad_for_animal(geographies=geographies)
 
         MatrixBase.__init__(self, name=name, row_indices=self.yaad_table.index,
--- OptSandbox/tables/MatrixSand.py
+++ OptSandbox/tables/MatrixSand.py
@@ -5,7 +5,7 @@ class MatrixSand(MatrixBase):
     def __init__(self, name='', geographies=None, agencies=None, queries=None):
         """Sub-class for Septic, Agriculture, Natural, and Developed (SAND) BMPs"""
 
-        # Load source with agencies, geo, etc. ("Ya'ad") table is generated.
+        # "Ya'ad" (load sources, agencies, geo, units, amounts, etc.) table is generated.
         self.yaad_table = queries.loadsources.get_yaad_for_sand(geographies=geographies, agencies=agencies)
 
         MatrixBase.__init__(self, name=name, row_indices=self.yaad_table.index,
