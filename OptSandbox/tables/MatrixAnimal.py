11bbbf7bd14d9bc1f3d08ef88ed1f2f456ab2612
dkauf42 <dkauf42@gmail.com>
2018-03-06 15:32:50 -0500

    fix hard upper bounds for animals to be specific to FIPS and animalname

--- OptSandbox/tables/MatrixAnimal.py
+++ OptSandbox/tables/MatrixAnimal.py
@@ -13,16 +13,20 @@ class MatrixAnimal(MatrixBase):
                             column_names=queries.tables.srcdata.allbmps_shortnames)
 
     def identifyhardupperbounds(self):
-        print("\nMatrixAnimal:identifyhardupperbounds: ya'ad table...")
-        print(self.yaad_table.head())
-        old_keys = [x for x in self.yaad_table['AnimalName']]
+        old_key_animal = [x for x in self.yaad_table['AnimalName']]
+        old_key_fips = [x for x in self.yaad_table['FIPS']]
         new_vals = [x for x in self.yaad_table['AnimalUnits']]
-        replace_vals = dict(zip(old_keys, new_vals))
+        replace_vals = dict(zip(zip(old_key_animal, old_key_fips), new_vals))
 
-        indexindex = self.eligibleparametermatrix.index.names.index('AnimalName')
+        index_animal = self.eligibleparametermatrix.index.names.index('AnimalName')
+        index_fips = self.eligibleparametermatrix.index.names.index('FIPS')
         for index, row in tqdm(self.eligibleparametermatrix.iterrows(), total=len(self.eligibleparametermatrix.index)):
             # iterate through the load sources
-            animalname = row.name[indexindex]
-            hub_value = replace_vals[animalname]
-            print('row animal name is <%s>, so replacing ones with %f' % (animalname, hub_value))
+            animalname = row.name[index_animal]
+            fipscode = row.name[index_fips]
+
+            hub_value = replace_vals[(animalname, fipscode)]
+            #print('row <animalname: %s> \n'
+            #      '    <fips:       %s>\n '
+            #      '     so replacing ones with %f' % (animalname, fipscode, hub_value))
             row[row == 1] = hub_value
