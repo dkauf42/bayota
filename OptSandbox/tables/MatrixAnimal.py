07055eacc3f33951bf4342b7608dce6018d10164
dkauf42 <dkauf42@gmail.com>
2018-03-19 12:21:48 -0400

    remove unused and commented code

--- OptSandbox/tables/MatrixAnimal.py
+++ OptSandbox/tables/MatrixAnimal.py
@@ -25,26 +25,3 @@ class MatrixAnimal(MatrixBase):
         hlb_table = self.eligibleparametermatrix.values * 0
         self.hardlowerboundmatrix = pd.DataFrame(hlb_table, index=self.eligibleparametermatrix.index,
                                                             columns=self.eligibleparametermatrix.columns)
-
-        # old_key_animal = [x for x in self.yaad_table.index.get_level_values('AnimalName')]
-        # old_key_fips = [x for x in self.yaad_table.index.get_level_values('FIPS')]
-        # new_vals = [x for x in self.yaad_table['AnimalUnits']]
-        # replace_vals = dict(zip(zip(old_key_animal, old_key_fips), new_vals))
-        #
-        # index_animal = self.eligibleparametermatrix.index.names.index('AnimalName')
-        # index_fips = self.eligibleparametermatrix.index.names.index('FIPS')
-        #
-        # self.yaad_table.to_csv('./output/testcompare_animal_yaadtable.csv')
-        # self.eligibleparametermatrix.to_csv('./output/testcompare_animal_eligibilitymatrix.csv')
-        #
-        # for index, row in tqdm(self.eligibleparametermatrix.iterrows(),
-        #                        total=len(self.eligibleparametermatrix.index), file=sys.stdout):
-        #     # iterate through the load sources
-        #     animalname = row.name[index_animal]
-        #     fipscode = row.name[index_fips]
-        #
-        #     hub_value = replace_vals[(animalname, fipscode)]
-        #     #print('row <animalname: %s> \n'
-        #     #      '    <fips:       %s>\n '
-        #     #      '     so replacing ones with %f' % (animalname, fipscode, hub_value))
-        #     row[row == 1] = hub_value
--- OptSandbox/tables/MatrixSand.py
+++ OptSandbox/tables/MatrixSand.py
@@ -27,34 +27,3 @@ class MatrixSand(MatrixBase):
         hlb_table = self.eligibleparametermatrix.values * 0
         self.hardlowerboundmatrix = pd.DataFrame(hlb_table, index=self.eligibleparametermatrix.index,
                                                             columns=self.eligibleparametermatrix.columns)
-
-
-        # loop_start_time = timeit.default_timer()
-        # old_key_lrseg = [x for x in self.yaad_table.index.get_level_values('LandRiverSegment')]
-        # old_key_agency = [x for x in self.yaad_table.index.get_level_values('Agency')]
-        # old_key_loadsrc = [x for x in self.yaad_table.index.get_level_values('LoadSource')]
-        # new_vals = [x for x in self.yaad_table['Amount']]
-        # replace_vals = dict(zip(zip(old_key_lrseg, old_key_agency, old_key_loadsrc), new_vals))
-        #
-        # index_lrseg = self.eligibleparametermatrix.index.names.index('LandRiverSegment')
-        # index_agency = self.eligibleparametermatrix.index.names.index('Agency')
-        # index_loadsrc = self.eligibleparametermatrix.index.names.index('LoadSource')
-        #
-        # self.yaad_table.to_csv('./output/testcompare_sand_yaadtable.csv')
-        # self.eligibleparametermatrix.to_csv('./output/testcompare_sand_eligibilitymatrix.csv')
-        #
-        # for index, row in tqdm(self.eligibleparametermatrix.iterrows(),
-        #                        total=len(self.eligibleparametermatrix.index), file=sys.stdout):
-        #     # iterate through the load sources
-        #     lrseg = row.name[index_lrseg]
-        #     agency = row.name[index_agency]
-        #     loadsrc = row.name[index_loadsrc]
-        #
-        #     hub_value = replace_vals[(lrseg, agency, loadsrc)]
-        #     #print('row <lrseg:   %s>\n'
-        #     #      '    <agency:  %s>\n '
-        #     #      '    <loadsrc: %s>\n '
-        #     #      '     so replacing ones with %f' % (lrseg, agency, loadsrc, hub_value))
-        #     row[row == 1] = hub_value
-        # self.eligibleparametermatrix.to_csv('./output/testcompare_sand_eligibilitymatrixcalc.csv')
-        # print("MatrixSand:identifyhardupperbounds: Loop calc time", timeit.default_timer() - loop_start_time)
