cd4789c712188cae01ea2604fe7a24d243c4d6f0
dkaufman <dkauf42@gmail.com>
2018-03-19 19:13:43 -0400

    add file to query SQL database

--- sandbox/util/pullfromdb.py
+++ sandbox/util/pullfromdb.py
@@ -1,1 +1,26 @@
-import pymssql
+import pyodbc
+
+server = 'SQL2D'
+database = 'master'
+username = 'dkaufman'
+driver = '{SQL Server Native Client 11.0}'
+port = '1433'
+
+cnn = pyodbc.connect('Driver=' + driver +
+                     ';Server=' + server +
+                     ';Database=' + database +
+                     ';Trusted_Connection=yes')
+
+#  cnn = pyodbc.connect('DRIVER=' + driver +
+#                      ';SERVER=' + server +
+#                      ';PORT=' + port +
+#                      ';DATABASE=' + database +
+#                      ';UID=' + username +
+#                      ';PWD=' + password)
+# cnn = pyodbc.connect(driver='{SQL Server}',
+#                      server=server,
+#                      database=database,
+#                      user=username,
+#                      password=password)
+cursor = cnn.cursor()
+cursor.execute('SELECT COUNT(ScenarioBuilderV3MetaData')
