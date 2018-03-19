008fee6fa3f9e2677b77dac211c7cea60b854aa4
dkauf42 <dkauf42@gmail.com>
2018-03-19 18:38:11 -0400

    add readme and setup files to project

---
+++ README.md
@@ -0,0 +1,28 @@
+# README #
+
+This README documents the steps necessary to get this application up and running.
+
+### What is this repository for? ###
+
+* OptSandbox by Daniel Kaufman
+* Version 0.1.0 (first version)
+
+### How do I get set up? ###
+
+* Summary of set up
+* Configuration
+* Dependencies
+* Database configuration
+* How to run tests
+* Deployment instructions
+
+### Contribution guidelines ###
+
+* Writing tests
+* Code review
+* Other guidelines
+
+### Who do I talk to? ###
+
+* Repo owner or admin
+* Other community or team contact
---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+#with open('LICENSE') as f:
+#    license = f.read()
+
+setup(
+    name='OptSandbox',
+    version='0.1.0',
+    description='Python package to create CAST scenarios for cost optimization',
+    long_description=readme,
+    author='Daniel Kaufman',
+    author_email='dkaufman@chesapeakebay.net',
+    url='https://gitlab.com/daka42',
+    license=license,
+    packages=find_packages(exclude=('docs', 'documentation_ppts',
+                                    'sphinx_doc', 'sphinx_guides', 'test'))
+)
