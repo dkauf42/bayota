91c0ee6f9178d8ca851129d67d1bf8ac33915d00
dkauf42 <dkauf42@gmail.com>
2018-03-23 12:38:43 -0400

    cosmetic

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 with open('README.md') as f:
@@ -18,20 +18,16 @@ install_requires = ['numpy==1.14.2',
                     'xlrd==1.1.0',
                     ]
 
-setup(
-    name='OptSandbox',
-    version='0.1.0',
-    description='Python package to create CAST scenarios for cost optimization',
-    long_description=readme_text,
-    author='Daniel Kaufman',
-    author_email='dkaufman@chesapeakebay.net',
-    url='https://gitlab.com/daka42',
-    license=license_text,
-    packages=['sandbox', 'data', 'temp', 'output'],
-    include_package_data=True,
-    install_requires=install_requires,
-    test_suite="sandbox.tests"
-)
-
-#packages = find_packages(exclude=['docs', 'documentation_ppts',
-#                                  'sphinx_doc', 'sphinx_guides', 'test'])
+setup(name='OptSandbox',
+      version='0.1.0',
+      description='Python package to create CAST scenarios for cost optimization',
+      long_description=readme_text,
+      author='Daniel Kaufman',
+      author_email='dkaufman@chesapeakebay.net',
+      url='https://gitlab.com/daka42',
+      license=license_text,
+      packages=['sandbox', 'data', 'temp', 'output'],
+      include_package_data=True,
+      install_requires=install_requires,
+      test_suite="sandbox.tests"
+      )
