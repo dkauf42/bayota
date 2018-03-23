da84486619aad822c63af10648c9a973b199fe66
dkauf42 <dkauf42@gmail.com>
2018-03-23 17:53:24 -0400

    create function to explore the structure of the source data tables

---
+++ sandbox/sqltables/sourcetable_explorer.py
@@ -0,0 +1,21 @@
+""" For each table in Source, print out a summary (column headers and number of rows) """
+import os
+import pandas as pd
+
+directory = os.path.abspath('../../data_sql/test_source/')
+
+for filename in sorted(os.listdir(directory)):
+    if filename.endswith(".csv"):
+        filepath = os.path.join(directory, filename)
+        filename = os.path.splitext(filename)[0]
+
+        # Count the number of rows in the csv
+        fileObj = open(filepath)
+        row_count = sum(1 for row in fileObj)
+
+        # Get the column headers from the csv
+        headers = pd.read_csv(filepath, nrows=1).columns.values
+        print('%s (%d rows): %s\n' % (filename, row_count, ', '.join(headers)))
+        continue
+    else:
+        continue
