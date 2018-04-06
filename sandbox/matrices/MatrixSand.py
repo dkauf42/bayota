8ccfc6053912ca7cc03e8e1b0349f51d5e9ec29a
dkauf42 <dkauf42@gmail.com>
2018-04-06 16:44:17 -0400

    begin switching the yaad_table generation to using TblJeeves

--- sandbox/matrices/MatrixSand.py
+++ sandbox/matrices/MatrixSand.py
@@ -1,17 +1,28 @@
 import numpy as np
 import pandas as pd
 from sandbox.matrices.MatrixBase import MatrixBase
+from sandbox.tables.TblJeeves import TblJeeves
 
 
 class MatrixSand(MatrixBase):
-    def __init__(self, name='', geographies=None, agencies=None, queries=None):
+    def __init__(self, name='', geographies=None, agencies=None, sectors=None, queries=None):
         """Sub-class for Septic, Agriculture, Natural, and Developed (SAND) BMPs"""
 
         # "Ya'ad" (load sources, agencies, geo, units, amounts, etc.) table is generated.
-        self.yaad_table = queries.loadsources.get_yaad_for_sand(geographies=geographies, agencies=agencies)
+        #self.yaad_table = queries.loadsources.get_yaad_for_sand(geographies=geographies, agencies=agencies)
 
+        lrsegids = queries.lrsegids_from(lrsegnames=geographies)
+        agencyids = queries.agencyids_from(agencycodes=agencies)
+        lrsegids = queries.sectorids_from(sectornames=agencies)
+
+        self.yaad_table = queries.loadsources_from_lrseg_agency_sector(lrsegs=geographies,
+                                                                       agencies=agencies,
+                                                                       sectors=sectors)
+
+        # TODO: yaad_table needs have multi-index or multiple dimensions!
+        print(self.yaad_table)
         MatrixBase.__init__(self, name=name, row_indices=self.yaad_table.index,
-                            column_names=queries.tables.srcdata.allbmps_shortnames)
+                            column_names=queries.all_bmpnames())
 
     def identifyhardupperbounds(self):
         #numpy_start_time = timeit.default_timer()
