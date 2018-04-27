88954f02ed5c38144cdf427a3c4e846362f7ecc0
dkauf42 <dkauf42@gmail.com>
2018-04-27 14:59:52 -0400

    fix randomization method in the Single subclass of ScenarioMaker
    Former-commit-id: 1c9f25ef3b698172c9b91cb64ebe6e4e86f475a2
    Former-commit-id: 65c381c39f4ec0d3af099c6b3bb07d61f0b89ec0

--- sandbox/util/scenariomaker/makers/single.py
+++ sandbox/util/scenariomaker/makers/single.py
@@ -8,14 +8,14 @@ class Single(Maker):
     def randomize_betweenbounds(self):
         """Generate random integers for each non-empty (Geo, Agency, Source, BMP) coordinate
         """
-        self.landnametable['amount'] = self._randomvaluesbetween(lower=self.landnametable['lowerbound'].values,
-                                                                 upper=self.landnametable['upperbound'].values)
-        self.animalnametable['amount'] = self._randomvaluesbetween(lower=self.animalnametable['lowerbound'].values,
+        self.animalnametable['Amount'] = self._randomvaluesbetween(lower=self.animalnametable['lowerbound'].values,
                                                                    upper=self.animalnametable['upperbound'].values)
-        self.manurenametable['amount'] = self._randomvaluesbetween(lower=self.manurenametable['lowerbound'].values,
+        self.landnametable['Amount'] = self._randomvaluesbetween(lower=self.landnametable['lowerbound'].values,
+                                                                 upper=self.landnametable['upperbound'].values)
+        self.manurenametable['Amount'] = self._randomvaluesbetween(lower=self.manurenametable['lowerbound'].values,
                                                                    upper=self.manurenametable['upperbound'].values)
 
-        # rand_matrix = self._randomvaluesbetween(lowermatrix=self.hardlowerboundmatrix.values,
-        #                                         uppermatrix=self.hardupperboundmatrix.values)
-        # self.scenariomatrix = pd.DataFrame(rand_matrix, index=self.hardupperboundmatrix.index,
-        #                                    columns=self.hardupperboundmatrix.columns)
+        # add to list of scenarios
+        self.scenarios_animal.append(self.animalnametable.copy().drop(columns=['lowerbound', 'upperbound']))
+        self.scenarios_land.append(self.landnametable.copy().drop(columns=['lowerbound', 'upperbound']))
+        self.scenarios_manure.append(self.manurenametable.copy().drop(columns=['lowerbound', 'upperbound']))
