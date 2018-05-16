787167616c41f71c77f6d7e8236de844a76b58f4
dkauf42 <dkauf42@gmail.com>
2018-05-16 12:52:53 -0400

    add check whether code is running on AWS EC2 instance
    Former-commit-id: 69ed55d477b773f9ebe1b4e905da553a525e1a28
    Former-commit-id: a138466db1f775bd2cbf4ad6a75e9ed94f3da400

--- sandbox/__init__.py
+++ sandbox/__init__.py
@@ -4,7 +4,29 @@ Module for cast_opt_tests
 
 import os
 
+# Check if running on AWS
+# import urllib
+from urllib.request import urlopen
+# html = urlopen("http://www.google.com/")
+
+meta = 'http://169.254.169.254/latest/meta-data' #/ami-id'
+# req = urllib.request(meta)
+try:
+    response = urlopen(meta).read()
+    if 'ami' in response:
+        _msg = 'I am in AWS running on {}'.format(response)
+    else:
+        _msg = 'I am in dev - no AWS AMI'
+except Exception as nometa:
+    _msg = 'no metadata, not in AWS'
+
+print(_msg)
+# import s3fs
+
+# s3 = s3fs.core.S3FileSystem(anon=False)
+
 _ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# _S3BUCKET = 's3://modeling-data.chesapeakebay.net/'
 
 
 def get_datadir(path=''):
