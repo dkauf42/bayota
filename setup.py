a194a7bc9b9c068487a2afc9322b94badffd5701
dkauf42 <dkauf42@gmail.com>
2018-09-13 14:18:53 -0400

    add setup.py and setup.cfg files for automated CI testing
    Former-commit-id: b619b33f5d2a3d5740732bd6d4e2452eb79a8695

---
+++ setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, Command
+
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    user_options = []
+
+    @staticmethod
+    def run():
+        os.system('rm -vrf ./build ./dist ./*.egg-info')
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+
+with open('README.md') as f:
+    readme_text = f.read()
+
+with open('LICENSE') as f:
+    license_text = f.read()
+
+install_requires = ['pytest'
+                    ]
+
+setup(name='OptEfficiencySubProblem',
+      version='0.0.1',
+      description='Python package to set up and solve an NLP for efficiency BMPs of CAST',
+      long_description=readme_text,
+      author='Daniel Kaufman',
+      author_email='dkaufman@chesapeakebay.net',
+      url='https://gitlab.com/daka42',
+      license=license_text,
+      packages=['src', 'jnotebooks'],
+      include_package_data=True,
+      install_requires=install_requires,
+      test_suite="src.tests",
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      cmdclass={'clean': CleanCommand}
+      )
