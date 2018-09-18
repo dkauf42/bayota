1e8731f19895d71af75dfb5ab2a1054bf4c6b05d
dkauf42 <dkauf42@gmail.com>
2018-09-18 10:37:27 -0400

    set up top-level definitions file to specify absolute root_dir path
    Former-commit-id: 14cc050b0c23e04f6ed86245f3a871cc2d9807cc
    Former-commit-id: d838acb294ae33f777002aecb03488be90200ec6

--- efficiencysubproblem/jnotebooks/importing_modules.py
+++ efficiencysubproblem/jnotebooks/importing_modules.py
@@ -1,10 +1,12 @@
 import os
+from definitions import ROOT_DIR
 from amplpy import AMPL, Environment
 
 # %pylab inline
 
-baseexppath = '/Users/Danny/Desktop/CATEGORIES/CAREER_MANAGEMENT/CRC_ResearchScientist_Optimization/Optimization_Tool/2_ExperimentFolder/'
-projectpath = os.path.join(baseexppath, 'efficiencysubproblem/')
-amplappdir = os.path.join(baseexppath, 'ampl/amplide.macosx64/')
+# baseexppath = '/Users/Danny/Desktop/CATEGORIES/CAREER_MANAGEMENT/CRC_ResearchScientist_Optimization/Optimization_Tool/2_ExperimentFolder/'
+
+projectpath = os.path.join(ROOT_DIR, 'efficiencysubproblem/')
+amplappdir = os.path.join(ROOT_DIR, 'ampl/amplide.macosx64/')
 
 # ampl = AMPL(Environment(amplappdir))
