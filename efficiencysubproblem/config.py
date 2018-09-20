e9c00aec8655643011b1fa7f13cce853a05f8b75
dkauf42 <dkauf42@gmail.com>
2018-09-20 16:16:43 -0400

    remove commented, unused ode that loads ampl environment
    Former-commit-id: 99e544daf59fe9d99f4a592fcb916cc86df995fc
    Former-commit-id: 7f731db3d132045adf52827a42d62dd4b5f6b230

--- efficiencysubproblem/config.py
+++ efficiencysubproblem/config.py
@@ -1,12 +1,6 @@
 import os
 from definitions import ROOT_DIR
-from amplpy import AMPL, Environment
-
-# %pylab inline
-
-# baseexppath = '/Users/Danny/Desktop/CATEGORIES/CAREER_MANAGEMENT/CRC_ResearchScientist_Optimization/Optimization_Tool/2_ExperimentFolder/'
+# from amplpy import AMPL, Environment
 
 PROJECT_DIR = os.path.join(ROOT_DIR, 'efficiencysubproblem/')
 AMPLAPP_DIR = os.path.join(ROOT_DIR, 'ampl/amplide.macosx64/')
-
-# ampl = AMPL(Environment(amplappdir))
