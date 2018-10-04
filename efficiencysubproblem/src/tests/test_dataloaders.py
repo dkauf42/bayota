8bbc7e623ac80fef524050e354bd33add4448f7a
dkauf42 <dkauf42@gmail.com>
2018-10-04 19:40:39 -0400

    update parameters passed to DataHandler classes to match base signature
    Former-commit-id: 2ab9ed3ef2dcca1bb4b645b460f160f0ab2d3d44
    Former-commit-id: be14ca84902efda10c0fbc48d100aaa8f9e2baee

--- efficiencysubproblem/src/tests/test_dataloaders.py
+++ efficiencysubproblem/src/tests/test_dataloaders.py
@@ -1,37 +1,43 @@
 import pytest
-# from efficiencysubproblem.src.data_handlers.lrseg import Lrseg
-from efficiencysubproblem.src.data_handlers.dataloader_types import *
-# from efficiencysubproblem.src.data_handlers.county import County
+
+from efficiencysubproblem.src.data_handling.interface import *
 
 
 def test_default_CountyWithCostConstraint_instantiation_ADAMSCounty():
-    yo = CountyWithCostConstraint(save2file=False, geolist=['Adams, PA'])
+    yo = DataHandlerCountyWithCostConstraint(savedata2file=False, geoentities=['Adams, PA'])
     # Verify the lrseg list is populated correctly
     assert 'N42001PU0_3000_3090' in yo.LRSEGS
 
 
 def test_default_CountyWithCostConstraint_instantiation_NUmberlandVACounty():
-    county = CountyWithCostConstraint(save2file=False, geolist=['Northumberland, VA'])
+    county = DataHandlerCountyWithCostConstraint(savedata2file=False, geoentities=['Northumberland, VA'])
     # Verify the lrseg list is populated correctly
     assert county.lrsegsetlist == ['N51133RL0_6450_0000', 'N51133RL0_6530_0000', 'N51133RL0_6501_0000', 'N51133PL0_6272_0000', 'N51133PL0_6271_0000', 'N51133PL0_6270_0000', 'N51133PL0_6140_0000']
 
 
 def test_default_CountyWithLoadReductionConstraint_instantiation():
-    yo = CountyWithLoadReductionConstraint(save2file=False, geolist=['Adams, PA'])
+    yo = DataHandlerCountyWithLoadReductionConstraint(savedata2file=False, geoentities=['Adams, PA'])
     # Verify the lrseg list is populated correctly
     assert 'N42001PU0_3000_3090' in yo.LRSEGS
 
 
 def test_default_LrsegWithCostConstraint_instantiation():
-    yo = LrsegWithCostConstraint(save2file=False, geolist=['N51133RL0_6450_0000'])
+    yo = DataHandlerLrsegWithCostConstraint(savedata2file=False, geoentities=['N51133RL0_6450_0000'])
     # Verify the lrseg list is populated correctly
     assert 'N51133RL0_6450_0000' in yo.LRSEGS
 
 
 def test_default_LrsegWithLoadReductionConstraint_instantiation():
-    yo = LrsegWithLoadReductionConstraint(save2file=False, geolist=['N51133RL0_6450_0000'])
+    yo = DataHandlerLrsegWithLoadReductionConstraint(savedata2file=False, geoentities=['N51133RL0_6450_0000'])
     # Verify the lrseg list is populated correctly
     assert 'N51133RL0_6450_0000' in yo.LRSEGS
+
+def test_default_LrsegWithLoadReductionConstraint_instantiation_inMontgomeryCountyMD():
+    yo = DataHandlerLrsegWithLoadReductionConstraint(savedata2file=False, geoentities=['N24031PM0_4640_4820'])
+    # N24031PM0_4640_4820 = Cabin John Creek, in Montgomery County
+
+    # Verify the lrseg list is populated correctly
+    assert 'N24031PM0_4640_4820' in yo.LRSEGS
 
 # def test_default_lrseg_instantiation():
 #     lrseg = Lrseg(save2file=False, geolist=['N51133RL0_6450_0000'])  # lrseg in Northumberland County, VA
