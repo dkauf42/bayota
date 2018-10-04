448f14fb72355c52842dc9814d5bdf904f6ba138
dkauf42 <dkauf42@gmail.com>
2018-10-04 13:39:00 -0400

    wrap print statements for dataloader_types in 'if verbose:' checks
    Former-commit-id: e3a109f738248cfd0a476838fb33a201ab4a9c7c
    Former-commit-id: 5a9b921f081cd6fba688c73fb8e962c7354a213e

--- efficiencysubproblem/src/data_handlers/dataloader_types.py
+++ efficiencysubproblem/src/data_handlers/dataloader_types.py
@@ -4,37 +4,48 @@ from .dataloader_loadconstraint_mixin import LoadConstraintMixin
 from .dataloader_county_mixin import CountyMixin
 from .dataloader_lrseg_mixin import LrsegMixin
 
+from efficiencysubproblem.config import verbose
+
 
 class CountyWithCostConstraint(CostConstraintMixin, CountyMixin, DataLoaderBase):
     def __init__(self, save2file=None, geolist=None):
+        if verbose:
+            print('CountyWithCostConstraint.__init__()')
+
         self.countysetlist = []
         self.countysetidlist = []
         self.COUNTIES = []
         self.CNTYLRSEGLINKS = []
 
         # super constructor
-        print('CountyWithCostConstraint.__init__()')
         DataLoaderBase.__init__(self, save2file=save2file, geolist=geolist)
 
 
 class LrsegWithCostConstraint(CostConstraintMixin, LrsegMixin, DataLoaderBase):
     def __init__(self, save2file=None, geolist=None):
+        if verbose:
+            print('LrsegWithCostConstraint.__init__()')
+
         # super constructor
-        print('LrsegWithCostConstraint.__init__()')
         DataLoaderBase.__init__(self, save2file=save2file, geolist=geolist)
 
 
 class LrsegWithLoadReductionConstraint(LoadConstraintMixin, LrsegMixin, DataLoaderBase):
     def __init__(self, save2file=None, geolist=None):
+        if verbose:
+            print('LrsegWithLoadReductionConstraint.__init__()')
+
         self.tau = None
 
         # super constructor
-        print('LrsegWithLoadReductionConstraint.__init__()')
         DataLoaderBase.__init__(self, save2file=save2file, geolist=geolist)
 
 
 class CountyWithLoadReductionConstraint(LoadConstraintMixin, CountyMixin, DataLoaderBase):
     def __init__(self, save2file=None, geolist=None):
+        if verbose:
+            print('CountyWithLoadReductionConstraint.__init__()')
+
         self.countysetlist = []
         self.countysetidlist = []
         self.COUNTIES = []
@@ -43,5 +54,4 @@ class CountyWithLoadReductionConstraint(LoadConstraintMixin, CountyMixin, DataLo
         self.tau = None
 
         # super constructor
-        print('CountyWithLoadReductionConstraint.__init__()')
         DataLoaderBase.__init__(self, save2file=save2file, geolist=geolist)
