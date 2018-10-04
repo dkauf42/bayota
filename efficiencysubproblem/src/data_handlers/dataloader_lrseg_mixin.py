bca0baa55e136d30d35917319823835e9dc79b50
dkauf42 <dkauf42@gmail.com>
2018-10-04 17:13:30 -0400

    combine mixins into a single dataloader .py file for compactness
    Former-commit-id: 446383c20efcde3c8cf362663e75244de3cadfbd
    Former-commit-id: 9e07ea1a6befb105ce0ca5e2965f93e596ba1c77

--- efficiencysubproblem/src/data_handlers/dataloader_lrseg_mixin.py
+++
@@ -1,6 +0,0 @@
-
-
-class DataLrsegMixin(object):
-    def _load_set_geographies(self, TblLandRiverSegment, geolist=None):
-        print('DataLrsegMixin._load_set_geographies()')
-        return self._load_set_lrsegs_from_lrseg_list(TblLandRiverSegment, geolist)
