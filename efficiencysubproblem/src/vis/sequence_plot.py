8705758a826f7023e2e7dfbdca03761b88239a26
dkauf42 <dkauf42@gmail.com>
2018-10-18 17:47:26 -0400

    add method for matplotlib visualization of tau sequence solutions
    Former-commit-id: d50c4a81a35156821a6fd3477717d76159783877
    Former-commit-id: 37626734f3961cd079fb01fcc0ddae4f97b11290

--- efficiencysubproblem/src/vis/sequence_plot.py
+++ efficiencysubproblem/src/vis/sequence_plot.py
@@ -1,3 +1,8 @@
+import os
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+import numpy as np
+
 import plotly.plotly as py
 import plotly.graph_objs as go
 
@@ -42,11 +47,28 @@ def plotly_costobj(df=None, savefig=True, savefilepathandname=None,
                    ylabel='Minimal Total Cost ($)'):
     if not savefilepathandname:
         savefig = False
+
+    df_feasib = df.loc[df['feasible'] == True, :]
+    df_infeas = df.loc[df['feasible'] == False, :]
+
     # Create a trace
-    trace = go.Scatter(x=df[xname],
-                       y=df['objective']
-                       )
-    data = [trace]
+    trace_feasible = go.Scatter(x=df_feasib[xname],
+                                y=df_feasib['objective'],
+                                mode='lines+markers',
+                                line=dict(
+                                    color='rgb(31, 119, 180)',
+                                    width=4,)
+                                )
+
+    trace_infeasible = go.Scatter(x=df_infeas[xname],
+                                  y=df_infeas['objective'],
+                                  mode='markers',
+                                  line=dict(
+                                      color='rgb(249, 120, 80)',
+                                      width=4,)
+                                  )
+
+    data = [trace_feasible, trace_infeasible]
 
     # Edit the layout
     layout = dict(title=title,
@@ -63,5 +85,101 @@ def plotly_costobj(df=None, savefig=True, savefilepathandname=None,
 
     if savefig:
         py.image.save_as(fig, filename=savefilepathandname, scale=5)
+
+    return fig
+
+
+def plotlib_costobj(df=None, savefig=True, savefilepathandname=None,
+                    xname='tau',
+                    title='Minimal Total Cost vs. Load Constraint',
+                    xlabel='Load Reduction (%) Lower Bound Constraint',
+                    ylabel='Minimal Total Cost ($)',
+                    secondaryxticklabels=None):
+    if not savefilepathandname:
+        savefig = False
+
+    rcParams['font.family'] = 'sans-serif'
+    rcParams['font.sans-serif'] = ['MS Reference Sans Serif']
+    rcParams['font.weight'] = 'normal'
+    rcParams['font.size'] = 12
+
+    df_feasib = df.loc[df['feasible'] == True, :]
+    df_infeas = df.loc[df['feasible'] == False, :]
+
+    fig, ax = plt.subplots()
+    for ii, data in enumerate([df_feasib, df_infeas]):
+        if ii == 0:
+            ax.plot(data[xname],
+                    data['objective'],
+                    color=[31/255, 119/255, 180/255],
+                    linestyle='-',
+                    linewidth=3,
+                    marker='o',
+                    label='feasible solutions',
+                    alpha=1.)
+        else:
+            ax.plot(data[xname],
+                    data['objective'],
+                    color=[249/255, 120/255, 80/255],
+                    linestyle='None',
+                    marker='o',
+                    label='infeasible solutions',
+                    alpha=0.7)
+
+    ax.grid(color=[200/255, 200/255, 200/255],
+            alpha=0.4, linestyle='-', linewidth=1)
+
+    ax.set_xticks(df[xname])
+    ax.set_xticklabels(df[xname])
+    plt.yticks(rotation=45)
+
+    # Hide the right and top spines
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+
+    ax.set_ylabel(ylabel, rotation=90)
+    ax.set_xlabel(xlabel)
+    ax.set_title(title)
+
+    ax.legend(frameon=False, loc='upper left', ncol=1)
+
+    if secondaryxticklabels is not None:
+        ax2 = ax.twiny()
+        axTicks = ax.get_xticks()
+        ax2Ticks = axTicks
+
+        def tick_function(X):
+            return ["%.1f" % z for z in X]
+
+        ax2.set_xticks(ax2Ticks)
+        ax2.set_xbound(ax.get_xbound())
+
+        ax2.set_xticklabels(tick_function(secondaryxticklabels))
+        ax2.tick_params(axis='x', which='major', labelsize=9)
+
+        # Add some extra space for the second axis at the bottom
+        plt.subplots_adjust(bottom=0.05)
+        # ax.xaxis.labelpad = 30
+
+        # Move twinned axis ticks and label from top to bottom
+        ax2.xaxis.set_ticks_position("bottom")
+        ax2.xaxis.set_label_position("bottom")
+        # Offset the twin axis below the host
+        ax2.spines["bottom"].set_position(("axes", -0.2))
+        # Turn on the frame for the twin axis, but then hide all
+        # but the bottom spine
+        ax2.set_frame_on(True)
+        ax2.patch.set_visible(False)
+        for k, sp in ax2.spines.items():
+            sp.set_visible(False)
+        ax2.spines["bottom"].set_visible(True)
+
+        plt.setp(ax2.xaxis.get_majorticklabels(), rotation=45)
+
+    plt.show()
+    if savefig:
+        fig.savefig(savefilepathandname, dpi=300,
+                    bbox_inches='tight', transparent=True)
 
     return fig
