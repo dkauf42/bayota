adb9ccd8eebd5b7e54f492c1a88254b6e6412bfe
dkauf42 <dkauf42@gmail.com>
2018-10-23 14:35:01 -0400

    remove log dir from efficiencysubproblem package; remove unused comments
    Former-commit-id: 0e4615d52a18fcb90bc58c78d29703964c1d24c6
    Former-commit-id: 89bf0900457f783219e800f5ef87ed1586e426d0

--- efficiencysubproblem/config.py
+++ efficiencysubproblem/config.py
@@ -1,22 +1,3 @@
 import os
 
-# import logging
-# import logging.config
-
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# logconfig_file = (os.path.dirname(PROJECT_DIR), 'logging_config.cfg')
-# logfilename = os.path.join(PROJECT_DIR, 'log', 'efficiencysubproblem_debug.log')
-#
-#
-# def set_up_logger():
-#     logging.config.fileConfig(logconfig_file, defaults={'logfilename': logfilename},
-#                               disable_existing_loggers=False)
-#
-#
-# class MyLogFormatter(logging.Formatter):
-#     def format(self, record):
-#         location = '%s.%s:%s' % (record.name, record.funcName, record.lineno)
-#         msg = '%s %-100s %-8s %s' % (self.formatTime(record), location, record.levelname, record.msg)
-#         record.msg = msg
-#         return super(MyLogFormatter, self).format(record)
