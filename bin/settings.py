a22da87d76d623250bf382e1eb0b0fd36b35cefb
dkauf42 <dkauf42@gmail.com>
2018-10-23 13:27:53 -0400

    check if output dir in settings.py exists, and make a graphics subdir
    Former-commit-id: 8d09d9801ca46d9ada1ed191bb1d88c388a33242
    Former-commit-id: f597caed441e0ef33b39a3a428e574eda3621694

--- bin/settings.py
+++ bin/settings.py
@@ -1,12 +1,21 @@
 from datetime import datetime
 import os
 
-outputdir_toplevel = "/home/xxx/"
+outputdir_toplevel = "/Users/Danny/"
 
-today = datetime.now()
-if today.hour < 12:
-    h = "00"
-else:
-    h = "12"
 
-os.mkdir(outputdir_toplevel + today.strftime('%Y%m%d') + h)
+def get_outdir_path():
+    today = datetime.now()
+    if today.hour < 12:
+        h = "00"
+    else:
+        h = "12"
+    dirname = os.path.join(outputdir_toplevel, 'temp_bayota_out_' + today.strftime('%Y%m%d') + h)
+    os.makedirs(dirname, exist_ok=True)
+    return dirname
+
+
+def get_graphics_path():
+    dirname = os.path.join(get_outdir_path(), 'graphics')
+    os.makedirs(dirname, exist_ok=True)
+    return dirname
