014834fc418419c7dcd955b479bf8a5dcf607e9f
dkauf42 <dkauf42@gmail.com>
2018-11-02 13:31:24 -0400

    organize bin/ by adding cli/ and python_scripts/ subdirectories
    Former-commit-id: 2cb0e8421c4c8e61f76692394e2ca31f8cb52340
    Former-commit-id: 2aa5f862fb06cde1be2167954d21fb18b989b602

--- bin/python_scripts/run_with_scientific_mode.py
+++ bin/python_scripts/run_with_scientific_mode.py
@@ -16,12 +16,14 @@ from efficiencysubproblem.src.solution_handling.solutionhandler import SolutionH
 graphicsdir = get_graphics_dir()
 
 #%%
-geoent = 'Adams, PA'
-s = Study(objectivetype='loadreductionmax',
-          geoscale='county', geoentities=[geoent])
 
-countyname = geoent.split(',')[0]
-stateabbrev = geoent.split(',')[1]
+lrsegs_for_annearundel = ['N24003WM0_3966_0000'] #['N24003WL0_4390_0000', 'N24003WL0_4424_0000', 'N24003WL0_4601_0000', 'N24003WL0_4393_0000']  #['N24003XU3_4650_0001', 'N24003XU2_4480_4650', 'N24003XL3_4710_0000', 'N24003XL3_4711_0000', 'N24003XL3_4712_0000', 'N24003XL3_4713_0000', 'N24003XL3_4950_0000', 'N24003XU2_4270_4650', 'N24003WL0_4420_0000', 'N24003WL0_4421_0000', 'N24003WL0_4422_0000', 'N24003WL0_4423_0000', 'N24003WL0_4600_0000', 'N24003WL0_4602_0000', 'N24003WL0_4603_0000', 'N24003WL0_4770_0000', 'N24003WL0_4771_0000', 'N24003WL0_4772_0000', 'N24003WM0_3961_0000', 'N24003WM0_3962_0000', 'N24003WM0_3963_0000', 'N24003WM3_4060_0001', 'N24003WL0_4425_0000', 'N24003WL0_4394_0000', 'N24003WL0_4391_0000', 'N24003WL0_4392_0000']
+# geoent = 'Anne Arundel, MD'
+s = Study(objectivetype='costmin',
+          geoscale='lrseg', geoentities=lrsegs_for_annearundel) #[geoent]) #lrsegs_for_annearundel)  #[geoent])
+
+# countyname = geoent.split(',')[0]
+# stateabbrev = geoent.split(',')[1]
 
 #%%
 # mdl = s.modelhandler.model
@@ -32,17 +34,25 @@ stateabbrev = geoent.split(',')[1]
 # print(sum((mdl.phi[l, lmbda, 'N'] * mdl.T[l, lmbda]) for lmbda in mdl.LOADSRCS))
 
 #%%
+import pyomo.environ as oe
+s.modelhandler.model.phi.pprint()
+
+# for s.modelhandler.model.component(x)
+
+#%%
+import numpy as np
 (solver_output_filepaths,
  solution_csv_filepath,
  mdf,
  solution_objective,
- feasibility_list) = s.go_constraintsequence(list(range(100000, 500000, 100000)))
+feasibility_list) = s.go_constraintsequence(list(np.arange(0.01, 0.1, 0.01)))
+ # feasibility_list) = s.go_constraintsequence(list(range(0, 5000000, 1000000)))
 
 #%%
 # log_infeasible_constraints(s.modelhandler.model)
 
 #%%
-constraintvar = 'totalcostupperbound'
+constraintvar = 'tau'
 df = pd.read_csv(solution_csv_filepath)
 df_piv = SolutionHandler.make_pivot_from_solution_sequence(solution_csv_filepath=solution_csv_filepath,
                                                            constraint_sequencing_var=constraintvar)
@@ -53,7 +63,8 @@ from efficiencysubproblem.src.vis.sequence_plot import plotlib_costobj
 
 fig = plotlib_costobj(df=df_piv, xname=constraintvar,
                           savefilepathandname=os.path.join(graphicsdir,
-                                                           stateabbrev + countyname + '_tau1-10' + '_plotlibfig.png'))
+                                                           stateabbrev + countyname + '_' + constraintvar + '1-10' + '_plotlibfig.png'),
+                      backend=None)
                       #secondaryxticklabels=df_piv['N_pounds_reduced'])
 # py.iplot(fig, filename='styled-line')
 
