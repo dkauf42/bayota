922927ec50d2fa75896a01a921142140e5670c57
dkauf42 <dkauf42@gmail.com>
2018-11-05 12:50:05 -0500

    expand the parsing of study configuration files and add two subcommands
    Former-commit-id: 73ed1682ea563f59914e1094db8b7a67e01403cc
    Former-commit-id: f45644a993a7c8abf16fe8c1d1c834ce7f6012c2

--- bin/cli/conductor_cli.py
+++ bin/cli/conductor_cli.py
@@ -3,27 +3,26 @@
 """
 Example Usage:
     # Just command line arguments:
-    >>> ./bin/study_cli.py -o costmin -s county -e "Adams, PA" "Lancaster, PA"
+    >>> ./bin/conductor_cli.py -o costmin -s county -e "Adams, PA" "Lancaster, PA"
     
     # Just config file argument values
-    >>> ./bin/study_cli.py @study_config.cfg
+    >>> ./bin/conductor_cli.py -f study_config.ini
     
     # With a configuration file and command line arguments:
-    #   Note: config file (with @ symbol) must come before cli arguments,
-    #         if you want cli args to take precedence over config file values
-    >>> ./bin/study_cli.py @study_config.cfg -s county -e "Adams, PA" "Lancaster, PA"
+    >>> ./bin/conductor_cli.py -f study_config.ini -s county -e "Adams, PA" "Lancaster, PA"
 """
 import os
+import sys
 import ast
 import timeit
 import logging
 import argparse
-import textwrap
+import cloudpickle
 from configparser import ConfigParser
 
 from efficiencysubproblem.src.study import Study
 from efficiencysubproblem.src.solution_handling.solutionhandler import SolutionHandler
-from efficiencysubproblem.src.vis.sequence_plot import plotlib_costobj
+from efficiencysubproblem.src.vis.sequence_plot import plotlib_costobj, plotlib_loadreductionobj
 
 from bayota_settings.install_config import get_graphics_dir, set_up_logger
 
@@ -33,75 +32,104 @@ logger = logging.getLogger(__name__)
 graphicsdir = get_graphics_dir()
 
 
-def main(objectivetype, scale, entities, baseyear):
+def main(args):
+
+    objectivetype = args.objective
+    scale = args.scale
+    entities = args.entities
+    baseyear = args.baseyear
 
     start_time = timeit.default_timer()
 
-    s = Study(objectivetype=objectivetype,
-              geoscale=scale, geoentities=entities)
+    savepath = 'saved_instance.pickle'
 
-    # countyname = g.split(',')[0]
-    # stateabbrev = g.split(',')[1]
+    if args.command == 'createinstance':
 
-    (solver_output_filepaths,
-     solution_csv_filepath,
-     mdf,
-     solution_objective,
-     feasibility_list) = s.go_constraintsequence(list(range(1, 5)))
+        # Create model instance
+        s = Study(objectivetype=objectivetype,
+                  geoscale=scale, geoentities=entities)
 
-    constraintvar = 'tau'
-    df_piv = SolutionHandler.make_pivot_from_solution_sequence(solution_csv_filepath=solution_csv_filepath,
-                                                               constraint_sequencing_var=constraintvar)
+        with open(savepath, "wb") as f:
+            cloudpickle.dump(s, f)
 
-    fig = plotlib_costobj(df=df_piv, xname=constraintvar,
-                          savefilepathandname=os.path.join(graphicsdir,'tau1-10' + '_plotlibfig.png'),
-                          secondaryxticklabels=df_piv['N_pounds_reduced'],
-                          showplot=False)
+    elif args.command == 'solveinstance':
+
+        with open(savepath, "rb") as f:
+            s = cloudpickle.load(f)
+
+        if objectivetype == 'costmin':
+            constraint_list = list(range(1, 5))
+            constraintvar = 'tau'
+            plotfunction = plotlib_costobj
+        elif objectivetype == 'loadreductionmax':
+            constraint_list = list(range(100000, 500000, 100000))
+            constraintvar = 'tau'
+            plotfunction = plotlib_loadreductionobj
+        else:
+            raise ValueError('unknown objective type')
+
+        # Solve
+        (solver_output_filepaths,
+         solution_csv_filepath,
+         mdf,
+         solution_objective,
+         feasibility_list) = s.go_constraintsequence(constraint_list)
+
+        df_piv = SolutionHandler.make_pivot_from_solution_sequence(solution_csv_filepath=solution_csv_filepath,
+                                                                   constraint_sequencing_var=constraintvar)
+
+        fig = plotfunction(df=df_piv, xname=constraintvar,
+                              savefilepathandname=os.path.join(graphicsdir, constraintvar + '_plotlibfig.png'),
+                              showplot=False)
 
     logger.info('<Runner Loading Complete>')
     logger.info("Loading time %s" % (timeit.default_timer() - start_time))
     logger.info('<DONE>')
 
-    return None
+    return 0
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(prog='bayota_efficiency',
                                      formatter_class=argparse.RawDescriptionHelpFormatter,
-                                     description=textwrap.dedent('''\
-                                                 Create and run an optimization case
-                                                 --------------------------------
-                                                 1. Generate a Decision Space    
-                                                      * specify metadata
-                                                         - base condition
-                                                         - wastewatername data
-                                                         - cost profile
-                                                         - geography
-                                                      * specify free parameter groups
-                                                      * specify constraints
-                                                 2. Generate a Scenario
-                                                 '''),
                                      # Turn off help, so we print all options in response to -h
                                      add_help=False)
 
-    parser.add_argument("-c", "--conf_file",
+    # Arguments for top-level
+    parser.add_argument("-v", "--verbose", dest='verbose',
+                        action="count", default=0)
+
+    parser.add_argument("-f", "--conf_file", dest='conf_file',
                         help="Specify config file", metavar="FILE")
-    args, remaining_argv = parser.parse_known_args()
 
-    defaults = {"objective":None,
-                "scale": None,
-                "entities": None,
-                "baseyear": None}
+    opts, remaining_argv = parser.parse_known_args()
 
-    if args.conf_file:
+    # Defaults are specified for all of the configuration options.
+    studyspec_defaults = {"objective":None,
+                          "scale": None,
+                          "entities": None,
+                          "baseyear": None}
+    solveoptions_defaults = {"fileprintlevel": 1}
+    instancespec_defaults = {"constraint": 5}
+    output_defaults = {'wheretoput': 'hey'}
+
+    # The defaults are merged into one dictionary.
+    defaults = dict(studyspec_defaults, **solveoptions_defaults)
+    defaults.update(instancespec_defaults)
+    defaults.update(output_defaults)
+
+    # Configuration file entries are read to update default values.
+    if opts.conf_file:
         config = ConfigParser()
-        config.read([args.conf_file])
-        defaults.update(dict(config.items("Defaults")))
+        config.read([opts.conf_file])
 
-    print(defaults)
-    defaults['entities'] = ast.literal_eval(defaults['entities'])
-    print(defaults)
+        defaults.update(dict(config.items("StudySpecs")))
+        defaults.update(dict(config.items("SolveOptions")))
+        defaults.update(dict(config.items("InstanceSpecs")))
+        defaults.update(dict(config.items("OutputOptions")))
+
+        defaults['entities'] = ast.literal_eval(defaults['entities'])
 
     # Parse rest of arguments
     # Don't suppress add_help here so it will handle -h
@@ -109,31 +137,50 @@ if __name__ == '__main__':
         # Inherit options from config_parser
         parents=[parser]
     )
-    parser.set_defaults(**defaults)
-    parser.add_argument('-o', '--objective', dest='objective',
-                        choices=['costmin', 'loadreductionmax'], type=str,
-                        help='optimization objective type')
 
-    parser.add_argument('-s', '--scale', dest='scale',
-                        choices=['county', 'lrseg'], type=str,
-                        help="geographic scale string")
+    # Make subparsers for "conductor.py createinstance ..." and "conductor.py solveinstance ..."
+    subparsers = parser.add_subparsers(dest="command")
+    createinstance_parser = subparsers.add_parser("createinstance")
+    solveinstance_parser = subparsers.add_parser("solveinstance")
+    subparsers.required = True
 
-    parser.add_argument('-e', '--entities', dest='entities',
-                        type=str,
-                        nargs="*",
-                        help="list of geographic entity names")
+    createinstance_parser.set_defaults(**defaults)
+    solveinstance_parser.set_defaults(**defaults)
 
-    parser.add_argument('-y', '--baseyear', dest='baseyear',
-                        type=str,
-                        nargs="*",
-                        help="base condition (year)")
+    # The lower-level arguments are defined and added to the appropriate subparsers.
+    createinstance_parser.add_argument('-o', '--objective', dest='objective',
+                                       choices=['costmin', 'loadreductionmax'], type=str,
+                                       help='optimization objective type')
+    createinstance_parser.add_argument('-s', '--scale', dest='scale',
+                                       choices=['county', 'lrseg'], type=str,
+                                       help="geographic scale string")
+    createinstance_parser.add_argument('-e', '--entities', dest='entities',
+                                       type=str,
+                                       nargs="*",
+                                       help="list of geographic entity names")
+    createinstance_parser.add_argument('-y', '--baseyear', dest='baseyear',
+                                       type=str,
+                                       help="base condition (year)")
 
-    # args = parser.parse_args()
-    args = parser.parse_args(remaining_argv)
-    print(args)
-    main(objectivetype=args.objective,
-         scale=args.scale,
-         entities=args.entities,
-         baseyear=args.baseyear,
+    solveinstance_parser.add_argument('-i', '--instancefile', dest='instancefile',
+                                      type=str,
+                                      help="path to saved concrete model instance")
+    solveinstance_parser.add_argument('-c', '--constraint', dest='constraint',
+                                      help="constraint level")
 
-         )
+    # The new arguments are parsed and added to the top-level namespace
+    opts = parser.parse_args(remaining_argv, namespace=opts)
+    print(opts)
+
+    # Options in the config file are overwritten by command line arguments.
+    if opts.conf_file:
+        config = ConfigParser()
+
+        config.read([opts.conf_file])
+        config.set("StudySpecs", "objective", opts.objective)
+        config.set("StudySpecs", "scale", opts.scale)
+        with open(opts.conf_file, "w") as f:
+            config.write(f)
+
+    # The main function is called.
+    sys.exit(main(opts))
