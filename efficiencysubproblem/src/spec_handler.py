f2f440d82eaf97d04fcc4f41807863ce2119c860
dkauf42 <dkauf42@gmail.com>
2018-11-08 14:50:25 -0500

    add spec_handler file with functions to read (yaml) specification files
    Former-commit-id: 83e2b2ec8d5218e688040df750e32a4ea6db6ea8
    Former-commit-id: 38510533e62001dce97ffc172f8aa8c908cd7fe9

---
+++ efficiencysubproblem/src/spec_handler.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import yaml
+from argparse import ArgumentParser
+
+
+def read(spec_file):
+    adict = None
+    with open(spec_file, 'r') as stream:
+        try:
+            adict = yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    return adict
+
+
+def parse_cli_arguments():
+    """ Input arguments are parsed. """
+    parser = ArgumentParser()
+    one_or_the_other = parser.add_mutually_exclusive_group(required=True)
+    one_or_the_other.add_argument("-s", "--study_spec", dest="study_spec_file", default=None,
+                                  help="read in a study specification file")
+    one_or_the_other.add_argument("-e", "--experiment_spec", dest="experiment_spec_file", default=None,
+                                  help="read in an experiment specification file")
+    opts = parser.parse_args()
+
+    if not opts.experiment_spec_file:  # study spec was given
+        opts.specfile = opts.study_spec_file
+    else:  # experiment spec was given
+        opts.specfile = opts.experiment_spec_file
+
+    return opts
+
+
+if __name__ == '__main__':
+    opts = parse_cli_arguments()
+
+    adict = read(opts.specfile)
+
+    print(adict)
