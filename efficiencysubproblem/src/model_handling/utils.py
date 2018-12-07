f08b205c9be18a75884910ab673f8d2f186af2a1
dkauf42 <dkauf42@gmail.com>
2018-12-07 11:22:15 -0500

    add modify_model method that will do specified experiment setup actions
    Former-commit-id: 5c56a9539ed3127f3adb540b2209ee718403cf2a
    Former-commit-id: 1143776aab1d628abbd1664f59a205c2734d99b6

--- efficiencysubproblem/src/model_handling/utils.py
+++ efficiencysubproblem/src/model_handling/utils.py
@@ -1,6 +1,49 @@
 import pyomo.environ as pe
+from itertools import compress
+
+import logging
+logger = logging.getLogger('root')
 
 
 def extract_indexed_expression_values(indexed_expr):
     """Returns the values of an indexed expression (PYOMO object)."""
     return dict((ind, pe.value(val)) for ind, val in indexed_expr.items())  # changed for use with python3, from iteritems()
+
+
+def modify_model(model, actiondict=None):
+    if actiondict['action'] == 'add_component':
+        if actiondict['component_type'] == 'Param':
+            # logger.info(actiondict['args'])
+            # Set the model Object
+            setattr(model, actiondict['name'], pe.Param(**actiondict['args']))
+
+        # elif actiondict['component_type'] == 'Constraint':
+        #     print(actiondict['args'])
+        #     # Set the model Object
+        #     setattr(model, actiondict['name'], pe.Constraint(**actiondict['args']))
+    elif actiondict['action'] == 'fix_variable':
+        idxset = actiondict['index']['set']
+        idxval = actiondict['index']['value']
+        fix_value = actiondict['value']
+        # print(f'idxset: {idxset}')
+        # print(f'idxval: {idxval}')
+        # print(f'fix_value: {fix_value}')
+
+        # find which element in the index tuple do we need to compare
+        compbool = [idxset == st._name for st in model.x._index.set_tuple]
+
+        ii = 0
+        for idxtuple in model.component('x'):
+            compval = list(compress(idxtuple, compbool))[0]
+            if compval.lower() == idxval.lower():
+                ii += 1
+                model.x[idxtuple].fix(fix_value)
+
+        # Check whether any values were found
+        if ii == 0:
+            logger.info('model_handling.util.modify_model(): '
+                        'No Matching values found')
+        else:
+            logger.info('model_handling.util.modify_model(): '
+                        '%d values matching the index <%s> were fixed to %d' %
+                        (ii, idxval, fix_value))
