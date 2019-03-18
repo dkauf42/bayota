d5b655f86bee6fbff585b9efe2dccb97bb25feb9
dkauf42 <dkauf42@gmail.com>
2019-03-18 16:09:24 -0400

    add new_load expression to pyomo model
    Former-commit-id: 1e91ad0c1acd6fa146e4b46081f3a4e65bc56011
    Former-commit-id: e7884bf0da1330c21f891f5ee82509d2f4217add

--- efficiencysubproblem/src/model_handling/model_expressions.py
+++ efficiencysubproblem/src/model_handling/model_expressions.py
@@ -1,6 +1,20 @@
 import pyomo.environ as pe
 
 
+def total_cost_expr(mdl):
+    """ Total Cost Expression """
+    def total_cost_rule(model):
+        return sum([(model.c[b] * model.x[b, l, lmbda])
+                    if ((b, lmbda) in model.BMPSRCLINKS)
+                    else 0
+                    for l in model.LRSEGS
+                    for lmbda in model.LOADSRCS
+                    for b in model.BMPS])
+
+    mdl.total_cost_expr = pe.Expression(rule=total_cost_rule)
+    return mdl
+
+
 def original_load_expr(mdl):
     """ Original Load Expression (with lrsegs aggregated together) """
     def original_load_rule(model, p):
@@ -24,30 +38,9 @@ def original_load_for_each_lrseg_expr(mdl):
     return mdl
 
 
-def total_cost_expr(mdl):
-    """ Total Cost Expression """
-    def total_cost_rule(model):
-        return sum([(model.c[b] * model.x[b, l, lmbda])
-                    if ((b, lmbda) in model.BMPSRCLINKS)
-                    else 0
-                    for l in model.LRSEGS
-                    for lmbda in model.LOADSRCS
-                    for b in model.BMPS])
-
-    mdl.total_cost_expr = pe.Expression(rule=total_cost_rule)
-    return mdl
-
-
-def percent_reduction_expr(mdl):
-    """ Percent Relative Load Reduction (with lrsegs aggregated together) """
-
-    # The model parameter 'originalload[p]' is required for this expression.
-    mdl = original_load_expr(mdl)
-    # loading before any new BMPs have been implemented
-    mdl.originalload = pe.Param(mdl.PLTNTS,
-                                initialize=lambda m, p: m.original_load_expr[p])
-
-    def percent_reduction_rule(model, p):
+def new_load_expr(mdl):
+    """ New Load (with lrsegs aggregated together) """
+    def new_load_rule(model, p):
         newload = sum([model.phi[l, lmbda, p] * model.T[l, lmbda] *
                        pe.prod([(1 - sum([(model.x[b, l, lmbda] / model.T[l, lmbda]) * model.E[b, p, l, lmbda]
                                           if ((model.T[l, lmbda] > 1e-6) &
@@ -61,24 +54,15 @@ def percent_reduction_expr(mdl):
                        for l in model.LRSEGS
                        for lmbda in model.LOADSRCS])
 
-        return ((model.originalload[p] - newload) / model.originalload[p]) * 100
+        return newload
 
-    mdl.percent_reduction_expr = pe.Expression(mdl.PLTNTS, rule=percent_reduction_rule)
+    mdl.new_load_expr = pe.Expression(mdl.PLTNTS, rule=new_load_rule)
     return mdl
 
 
-def percent_reduction_for_each_lrseg_expr(mdl):
-    """ Percent Relative Load Reduction (quantified for each lrseg) """
-
-    # The model parameter 'originalload[l, p]' is required for this expression.
-    mdl = original_load_for_each_lrseg_expr(mdl)
-    # loading before any new BMPs have been implemented
-    mdl.originalload = pe.Param(mdl.LRSEGS,
-                                mdl.PLTNTS,
-                                initialize=lambda m, l, p: m.original_load_for_each_lrseg_expr[l, p])
-
-    # Relative load reductions must be greater than the specified target percentages (tau)
-    def percent_reduction_rule_for_each_lrseg(model, l, p):
+def new_load_for_each_lrseg_expr(mdl):
+    """ New Loa (quantified for each lrseg) """
+    def new_load_rule_for_each_lrseg(model, l, p):
         newload = sum([model.phi[l, lmbda, p] * model.T[l, lmbda] *
                        pe.prod([(1 - sum([(model.x[b, l, lmbda] / model.T[l, lmbda]) * model.E[b, p, l, lmbda]
                                           if ((model.T[l, lmbda] > 1e-6) &
@@ -91,6 +75,45 @@ def percent_reduction_for_each_lrseg_expr(mdl):
                                 for gamma in model.BMPGRPS])
                        for lmbda in model.LOADSRCS])
 
+        return newload
+
+    mdl.new_load_for_each_lrseg_expr = pe.Expression(mdl.PLTNTS,
+                                                     mdl.LRSEGS,
+                                                     rule=new_load_rule_for_each_lrseg)
+    return mdl
+
+
+def percent_reduction_expr(mdl):
+    """ Percent Relative Load Reduction (with lrsegs aggregated together) """
+
+    # The model parameters 'originalload[p]' and 'newload[p]' is required for this expression.
+    mdl = original_load_expr(mdl)
+    mdl = new_load_expr(mdl)
+    # loading before any new BMPs have been implemented
+    mdl.originalload = pe.Param(mdl.PLTNTS,
+                                initialize=lambda m, p: m.original_load_expr[p])
+
+    def percent_reduction_rule(model, p):
+        return ((model.originalload[p] - model.new_load_expr[p]) / model.originalload[p]) * 100
+
+    mdl.percent_reduction_expr = pe.Expression(mdl.PLTNTS, rule=percent_reduction_rule)
+    return mdl
+
+
+def percent_reduction_for_each_lrseg_expr(mdl):
+    """ Percent Relative Load Reduction (quantified for each lrseg) """
+
+    # The model parameter 'originalload[l, p]' is required for this expression.
+    mdl = original_load_for_each_lrseg_expr(mdl)
+    mdl = new_load_for_each_lrseg_expr(mdl)
+    # loading before any new BMPs have been implemented
+    mdl.originalload = pe.Param(mdl.LRSEGS,
+                                mdl.PLTNTS,
+                                initialize=lambda m, l, p: m.original_load_for_each_lrseg_expr[l, p])
+
+    # Relative load reductions must be greater than the specified target percentages (tau)
+    def percent_reduction_rule_for_each_lrseg(model, l, p):
+
         # Some lrsegs have 0 total load for some nutrients
         # E.g. N24031PM0_4640_4820 = Cabin John Creek, in Montgomery County
         # has 0 originalload for phosphorus.  This causes a ZeroDivisionError when Pyomo
@@ -98,7 +121,7 @@ def percent_reduction_for_each_lrseg_expr(mdl):
         # To avoid this, we set the percent reduction here to zero no matter what, since
         # we can assume newload never increases from zero to a positive value.
         if model.originalload[l, p] != 0:
-            temp = ((model.originalload[l, p] - newload) / model.originalload[l, p]) * 100
+            temp = ((model.originalload[l, p] - model.new_load_for_each_lrseg_expr[l, p]) / model.originalload[l, p]) * 100
         else:
             temp = 0
 
