acdd8cdd455c405e38825fbd7784bb014ff44e3a
dkauf42 <dkauf42@gmail.com>
2019-03-19 10:19:22 -0400

    update s3 folder date
    Former-commit-id: 8f041b2e77b1fa429a8d6b1106e03ff62fc708d3
    Former-commit-id: 54265cb067d49fd5cf9aac71c8d66e0974a16345

--- bin/scripts_by_level/run_step4_solveonetrial.py
+++ bin/scripts_by_level/run_step4_solveonetrial.py
@@ -165,7 +165,7 @@ def main(saved_model_file=None, model_modification_string=None, trial_name=None,
         else:
             # Move solution file to s3
             destination_name = 'optimization' + '/' \
-                               + 'for_kevin_20190318' + '/' \
+                               + 'for_kevin_20190319' + '/' \
                                + geography_entity_str + '/' \
                                + objective_and_constraint_str + '/' \
                                + solution_shortname
