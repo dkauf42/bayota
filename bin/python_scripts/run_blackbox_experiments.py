0b0f207d70bfb69130e57b947ac0aac9da7ea59b
dkauf42 <dkauf42@gmail.com>
2019-03-22 15:15:16 -0400

    first draft
    Former-commit-id: 7837b9b552203e6b820053b2af064b571cfd243e
    Former-commit-id: da0cf75330724a1122003674d355e3a867bb1373

---
+++ bin/python_scripts/run_blackbox_experiments.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""
+Example usage command:
+    ./bin/scripts_by_level/run_single_study.py --dryrun -f /Users/Danny/Desktop/CATEGORIES/CAREER_MANAGEMENT/CRC_ResearchScientist_Optimization/Optimization_Tool/2_ExperimentFolder/bayota/bin/study_specs/single_study_specs/adamsPA_0001.yaml
+"""
+
+import os
+import time
+import logging
+import cloudpickle
+from argparse import ArgumentParser
+
+from efficiencysubproblem.src.spec_handler import notdry
+from efficiencysubproblem.src.model_handling.utils import load_model_pickle
+from bin.run_scripts import run_step2_generatemodel
+
+from bayota_settings.base import get_output_dir, get_scripts_dir, get_model_instances_dir, \
+    set_up_logger, get_bayota_version, get_single_study_specs_dir, get_experiment_specs_dir
+
+logger = logging.getLogger('root')
+if not logger.hasHandlers():
+    set_up_logger()
+    logger = logging.getLogger(__name__)
+
+outdir = get_output_dir()
+
+
+def main(dryrun=False):
+    logprefix = '** Blackbox Experiment **: '
+    saved_model_file = os.path.join(get_model_instances_dir(),
+                                           'modelinstance_costmin_total_percentreduction_CalvertMD.pickle')
+
+    run_step2_generatemodel.main(model_spec_file='costmin_total_percentreduction',
+                                 geography_name='CalvertMD',
+                                 saved_model_file=saved_model_file,
+                                 dryrun=False, baseloadingfilename='2010NoActionLoads.csv')
+
+    mdlhandler = load_model_pickle(savepath=saved_model_file, dryrun=dryrun, logprefix=logprefix)
+
+    mdlhandler.model
+
