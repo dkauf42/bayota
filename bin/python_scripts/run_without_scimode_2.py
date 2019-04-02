4af1042d0c99b71328b3e3f3918d16a8cf56f499
dkauf42 <dkauf42@gmail.com>
2019-04-02 14:51:11 -0400

    add script for debugging
    Former-commit-id: 54944a2a1f35067777b9066aecd3d7f7b2c9decb
    Former-commit-id: 6d48dcf151fa9bf1d9ef0d6be03ebe0e66ae8dc5

---
+++ bin/python_scripts/run_without_scimode_2.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+import pyomo.environ as pe
+
+from efficiencysubproblem.src.model_handling import model_generator
+from efficiencysubproblem.src.solver_handling import solvehandler
+
+from castjeeves.src.jeeves import Jeeves
+
+
+jeeves = Jeeves()
+
+pd.set_option('display.float_format', lambda x: '{:,.1f}'.format(x))
+
+def numstr(number, decimalpoints: int) -> str:
+    """ Add commas, and restrict decimal places """
+    fmtstr = '{:,.%sf}' % str(decimalpoints)
+    return fmtstr.format(number)
+
+
+model_spec_file = '/Users/Danny/Desktop/CATEGORIES/CAREER_MANAGEMENT/CRC_ResearchScientist_Optimization/Optimization_Tool/2_ExperimentFolder/bayota/bin/specification_files/model_specs/costmin_total_Npercentreduction.yaml'
+# model_spec_file = '/Users/Danny/Desktop/CATEGORIES/CAREER_MANAGEMENT/CRC_ResearchScientist_Optimization/Optimization_Tool/2_ExperimentFolder/bayota/bin/specification_files/model_specs/Nloadreductionmax_totalcostupper.yaml'
+# mdlhandler = model_generator.ModelHandlerBase(model_spec_file=model_spec_file,
+#                                               geoscale='county',
+#                                               geoentities='Perry, PA',
+#                                               savedata2file=False,
+#                                               baseloadingfilename='2010NoActionLoads.csv')
+# mdl = mdlhandler.model
+
+from efficiencysubproblem.src.data_handling.interface import DataHandlerCounty
+
+dh = DataHandlerCounty(geolist=['Adams, PA'], baseloadingfilename='2010NoActionLoads_20190325.csv')
+
+# import math
+# for p in mdl.PLTNTS:
+#     if math.isinf(pe.value(mdl.original_load_expr[p])):
+#         print(f"Uh oh! original_load_expr for {p} is Inf")
+#     elif math.isnan(pe.value(mdl.original_load_expr[p])):
+#         print(f"Uh oh! original_load_expr for {p} is NaN")
+#     else:
+#         print("original_load_expr for %s is %s" % (p, numstr(pe.value(mdl.original_load_expr[p]), 2)))
