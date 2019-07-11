8ea67c1e50be1dacf51084a9230da35aec19754e
dkauf42 <dkauf42@gmail.com>
2019-07-11 18:05:34 -0400

    assign BMPGRPING a dictionary instead of a list of tuples

--- bayom_e/data_handling/datahandler_base.py
+++ bayom_e/data_handling/datahandler_base.py
@@ -226,8 +226,12 @@ class DataHandlerBase:
             pd.DataFrame(bmpgrpsetlist, columns=['BMPGRPS']).to_csv(os.path.join(self.instdatadir, 'data_BMPGRPS.tab'), sep=' ', index=False)
 
         # Get correspondences between bmp names and group names (and group ids)
+        # first, as dataframe column
         bmpgrpsdf['BMPGRPING'] = list(zip(bmpgrpsdf.bmpshortname, bmpgrpsdf.bmpgroupid))
-        self.BMPGRPING = list(bmpgrpsdf.BMPGRPING)
+        # then, also as a {grp: bmps} dictionary
+        grouped = bmpgrpsdf.groupby(['bmpgroupid'])
+        bmpgrping_dict = grouped['bmpshortname'].apply(lambda x: list(x)[0]).to_dict()
+        self.BMPGRPING = bmpgrping_dict
         if self.save2file:
             BMPGRPING_df_asseparatecolumns = bmpgrpsdf.loc[:, ['bmpshortname', 'bmpgroupid']].rename(
                 columns={'bmpshortname': 'BMPS',
