a957543bf6fcacceefe58c86cec38b817bf352a7
dkauf42 <dkauf42@gmail.com>
2019-07-11 18:36:13 -0400

    add lrseg parameter and dictionary keys to random dataplate generator

--- bayom_e/data_handling/data_interface.py
+++ bayom_e/data_handling/data_interface.py
@@ -34,7 +34,19 @@ def random_ints_with_sum(n):
     yield p + 1
 
 
-def make_random_bmp_groupings(num_bmps=8, num_bmpgroups=3, mingrpsize=1, maxgrpsize=10):
+def random_list_of_names(n, name_length=3, chars=string.ascii_uppercase) -> list:
+    # A list of random names is generated.
+    name_list = []
+    for i in range(n):
+        while True:  # generate a new name until we find one not already in the list.
+            newword = word_generator(size=name_length, chars=chars)
+            if newword not in name_list:  # don't add a duplicate
+                name_list.append(newword)
+                break
+    return name_list
+
+def make_random_bmp_groupings(pollutants_list, lrseg_list, loadsrc_list,
+                              num_bmps=8, num_bmpgroups=3, mingrpsize=1, maxgrpsize=10):
     """
 
     Args:
@@ -59,19 +71,18 @@ def make_random_bmp_groupings(num_bmps=8, num_bmpgroups=3, mingrpsize=1, maxgrps
                          f"by distributed {num_bmps} BMPs among {num_bmpgroups} groups")
 
     # A list of random bmp names is generated [of length 'num_bmps'].
-    bmp_list = []
-    for i in range(num_bmps):
-        while True:  # generate a new name until we find one not already in the list.
-            newword = word_generator(size=6)
-            if newword not in bmp_list:  # don't add a duplicate
-                bmp_list.append(newword)
-                break
+    bmp_list = random_list_of_names(n=num_bmps, name_length=6, chars=string.ascii_uppercase + string.ascii_lowercase)
 
     # generate random costs for each bmp
     tau_dict = {b: random.randint(0, 1000) for b in bmp_list}
 
     # generate a random effectiveness for each bmp
-    eta_dict = {b: round(random.random(), 2) for b in bmp_list}
+    eta_dict = {}
+    for b in bmp_list:
+        for p in pollutants_list:
+            for l in lrseg_list:
+                for u in loadsrc_list:
+                    eta_dict[(b, p, l, u)] = round(random.random(), 2)
 
     # The sizes for each group are determined randomly.
     grp_sizes = {i: mingrpsize for i in range(0, num_bmpgroups)}
@@ -101,7 +112,7 @@ def make_random_bmp_groupings(num_bmps=8, num_bmpgroups=3, mingrpsize=1, maxgrps
     return bmp_list, grp_sizes, bmpgroups_list, tau_dict, eta_dict
 
 
-def randomly_assign_grps_to_loadsources(num_loadsources, bmpgroups_list, minloadsrcgrpingsize=1,
+def randomly_assign_grps_to_loadsources(loadsrc_list, bmpgroups_list, minloadsrcgrpingsize=1,
                                         maxloadsrcgrpingsize=4):
     bmpgrouplist = bmpgroups_list.copy()
     num_bmpgroups = len(bmpgrouplist)
@@ -111,17 +122,9 @@ def randomly_assign_grps_to_loadsources(num_loadsources, bmpgroups_list, minload
                          f"can never be met with only {num_bmpgroups} BMP groups.  "
                          f"It is recommended to lower maxloadsrcgrpingsize to {num_bmpgroups}.")
 
-    # A list of random load sources names is generated.
-    loadsrc_list = []
-    for i in range(num_loadsources):
-        while True:  # generate a new name until we find one not already in the list.
-            newword = word_generator(size=3, chars=string.ascii_lowercase)
-            if newword not in loadsrc_list:  # don't add a duplicate
-                loadsrc_list.append(newword)
-                break
-
     # The sizes for each load source grouping are determined randomly.
-    loadsrc_sizes = np.random.choice(range(minloadsrcgrpingsize, maxloadsrcgrpingsize+1), size=num_loadsources).tolist()
+    loadsrc_sizes = np.random.choice(range(minloadsrcgrpingsize, maxloadsrcgrpingsize+1),
+                                     size=len(loadsrc_list)).tolist()
 
     # BMPGRPS are assigned to load sources using the specified sizes.
     grploadsrc_list = []
@@ -145,26 +148,33 @@ def randomly_assign_grps_to_loadsources(num_loadsources, bmpgroups_list, minload
         grploadsrc_list.append(LoadSrc(index=ls_index, name=loadsrc_list[ls_index],
                                        size=len(thisloadsourcesgrps), bmpgroups=thisloadsourcesgrps))
 
-    return loadsrc_list, loadsrc_sizes, grploadsrc_list
+    return loadsrc_sizes, grploadsrc_list
 
 
-def get_random_dataplate(name='nlp',
+def get_random_dataplate(name='nlp', num_lrsegs=1,
                          num_bmps=8, num_bmpgroups=3, num_loadsources=2,
-                         minbmpgrpsize=1, maxbmpgrpsize=10,
+                         minbmpgrpsize=1, maxbmpgrpsize=3,
                          minloadsrcgrpingsize=1, maxloadsrcgrpingsize=4,
                          savedata2file=False):
 
-    bmplist, grpsizes, bmpgrplist, tau_dict, eta_dict = make_random_bmp_groupings(num_bmps=num_bmps,
+    pollutants_list = ['N']
+    lrseg_list = random_list_of_names(n=num_lrsegs, name_length=19, chars=string.ascii_uppercase + string.digits)
+    loadsrc_list = random_list_of_names(n=num_loadsources, name_length=3, chars=string.ascii_lowercase)
+
+    bmplist, grpsizes, bmpgrplist, tau_dict, eta_dict = make_random_bmp_groupings(pollutants_list=pollutants_list,
+                                                                                  lrseg_list=lrseg_list,
+                                                                                  loadsrc_list=loadsrc_list,
+                                                                                  num_bmps=num_bmps,
                                                                                   num_bmpgroups=num_bmpgroups,
                                                                                   mingrpsize=minbmpgrpsize,
                                                                                   maxgrpsize=maxbmpgrpsize)
     # print(f"bmp group sizes are {grpsizes}.  sum={sum([s for _, s in grpsizes.items()])}")
     # print('\n')
 
-    loadsrclist, loadsrc_sizes, grploadsrc_list = randomly_assign_grps_to_loadsources(num_loadsources=num_loadsources,
-                                                                                      bmpgroups_list=bmpgrplist,
-                                                                                      minloadsrcgrpingsize=minloadsrcgrpingsize,
-                                                                                      maxloadsrcgrpingsize=maxloadsrcgrpingsize)
+    loadsrc_sizes, grploadsrc_list = randomly_assign_grps_to_loadsources(loadsrc_list=loadsrc_list,
+                                                                         bmpgroups_list=bmpgrplist,
+                                                                         minloadsrcgrpingsize=minloadsrcgrpingsize,
+                                                                         maxloadsrcgrpingsize=maxloadsrcgrpingsize)
     # print(f"load source sizes are {loadsrc_sizes}.  sum={sum(loadsrc_sizes)}")
     # print(grploadsrc_list)
 
@@ -176,18 +186,27 @@ def get_random_dataplate(name='nlp',
     bmpsrc_tuples = []
     [[[bmpsrc_tuples.append((b, ls.name)) for b in bmpgrplist[g].bmps] for g in ls.bmpgroups] for ls in grploadsrc_list]
 
+    # generate random acreages (alpha) and base loadings (phi)
+    alpha_dict = {}
+    phi_dict = {}
+    for l in lrseg_list:
+        for u in loadsrc_list:
+            alpha_dict[(l, u)] = random.randint(0, 10000)
+            for p in pollutants_list:
+                phi_dict[(l, u, p)] = random.randint(0, 2000)
+
     if name == 'nlp':
-        return NLP_DataPlate(PLTNTS=['N'],
-                             LRSEGS=['lrseg1'],
+        return NLP_DataPlate(PLTNTS=pollutants_list,
+                             LRSEGS=lrseg_list,
+                             LOADSRCS=loadsrc_list,
                              BMPS=bmplist,
                              BMPGRPS=[g.index for g in bmpgrplist],
                              BMPGRPING={g.index: g.bmps for g in bmpgrplist},
-                             LOADSRCS=loadsrclist,
                              BMPSRCLINKS=bmpsrc_tuples,
                              BMPGRPSRCLINKS=bmpgrpsrc_tuples,
                              theta=1,
-                             alpha={'lrseg1': 100},
-                             phi={'lrseg1': 500},
+                             alpha=alpha_dict,
+                             phi=phi_dict,
                              tau=tau_dict,
                              eta=eta_dict)
 
