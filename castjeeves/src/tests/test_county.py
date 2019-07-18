79a1f3f0003e70eb73a7c5ddf14fd897ab558e2a
dkauf42 <dkauf42@gmail.com>
2019-07-18 18:45:41 -0400

    cosmetic rename

--- castjeeves/src/tests/test_county.py
+++ castjeeves/src/tests/test_county.py
@@ -11,7 +11,7 @@ def resource_a(request):
     return County(sourcedata=source)
 
 
-def test_correct_countyid_query_from_list_of_countystatestrs(resource_a):
+def test_countyid_query_from_list_of_countystatestrs(resource_a):
     test_values = ['Adams, PA', 'Anne Arundel, MD']
     retval = resource_a.countyid_from_countystatestrs(getfrom=test_values).countyid.tolist()
     assert {11, 194} == set(retval)
