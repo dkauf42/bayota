b753ea9a14d70a3e509ff0b26e0c6a9ce3f4f0c8
dkauf42 <dkauf42@gmail.com>
2019-07-18 17:10:59 -0400

    add sourcehook tests for _map_using_a_sourcetable

--- castjeeves/src/tests/test_sourcehooks.py
+++ castjeeves/src/tests/test_sourcehooks.py
@@ -1,4 +1,5 @@
 import pytest
+import pandas as pd
 
 from ..jeeves import Jeeves
 from ..sourcehooks.sourcehooks import SourceHook
@@ -13,6 +14,30 @@ def resource_a(request):
 
 def test_singleconvert_returns_single_series(resource_a):
     pass
+
+
+def test_map_using_a_sourcetable_with_list(resource_a):
+    test_values = [10]
+    retval = resource_a._map_using_sourcetbl(test_values, tbl='TblAgency',
+                                             tocol='agencycode', fromcol='agencyid',
+                                             todict=False, flatten=False)
+    assert ('nps' in retval) and isinstance(retval, list)
+
+
+def test_map_using_a_sourcetable_with_series(resource_a):
+    test_values = pd.Series([10, 8])
+    retval = resource_a._map_using_sourcetbl(test_values, tbl='TblAgency',
+                                             tocol='agencycode', fromcol='agencyid',
+                                             todict=False, flatten=False)
+    assert ('nps' in retval.tolist()) and isinstance(retval, pd.Series)
+
+
+def test_map_using_a_sourcetable_with_dataframe(resource_a):
+    test_values = pd.DataFrame([10, 8], columns=['agencyid'])
+    retval = resource_a._map_using_sourcetbl(test_values, tbl='TblAgency',
+                                             tocol='agencycode', fromcol='agencyid',
+                                             todict=False, flatten=False)
+    assert ('nps' in retval['agencycode'].tolist()) and isinstance(retval, pd.DataFrame)
 
 
 def test_append_ids_to_table(resource_a):
