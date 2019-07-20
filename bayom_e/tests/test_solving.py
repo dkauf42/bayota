9160c06ba167570eac2be4490e8e6f0534a8be2e
dkauf42 <dkauf42@gmail.com>
2019-07-20 14:34:16 -0400

    use new build_model function and solverhandler.solve() in solving test

--- bayom_e/tests/test_solving.py
+++ bayom_e/tests/test_solving.py
@@ -1,24 +1,25 @@
 import os
 import pytest
 
-from bayom_e.model_handling import model_generator
+from bayom_e.model_handling.interface import build_model
 from bayom_e.solver_handling import solvehandler
 
 
 @pytest.fixture(scope='module')
-def costmin_modelhandler_for_singlelrseg(request):
+def costmin_model_for_singlelrseg(request):
     THIS_DIR = os.path.dirname(os.path.abspath(__file__))
     costmin_model_spec_path = os.path.join(THIS_DIR, 'costmin_total_Npercentreduction.yaml')
-    mdlhandler = model_generator.ModelHandlerBase(model_spec_file=costmin_model_spec_path,
-                                                  geoscale='lrseg',
-                                                  geoentities=['N51133RL0_6450_0000'],
-                                                  savedata2file=False,
-                                                  baseloadingfilename='2010NoActionLoads_20190325.csv')
-    return mdlhandler
+
+    model = build_model(model_spec_file=costmin_model_spec_path,
+                        geoscale='lrseg',
+                        geoentities=['N51133RL0_6450_0000'],
+                        savedata2file=False,
+                        baseloadingfilename='2010NoActionLoads_updated.csv')
+    return model
 
 
-def test_default_modelsolve_singlelrseg(costmin_modelhandler_for_singlelrseg):
-    mdlhandler = costmin_modelhandler_for_singlelrseg
-    solution_dict = solvehandler.basic_solve(modelhandler=mdlhandler, mdl=mdlhandler.model,
-                                             translate_to_cast_format=False)
-    assert solution_dict['feasible'] is True
+def test_default_modelsolve_singlelrseg(costmin_model_for_singlelrseg):
+    solvername = 'ipopt'
+    instance, results, feasible = solvehandler.solve(localsolver=True, solvername=solvername,
+                                                     instance=costmin_model_for_singlelrseg)
+    assert feasible is True
