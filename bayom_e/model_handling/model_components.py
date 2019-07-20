723c15fac34a432717faa3f4c7501a335f572fd6
dkauf42 <dkauf42@gmail.com>
2019-07-20 14:25:01 -0400

    add agencies and parcels to Available_Acres_Constraint

--- bayom_e/model_handling/model_components.py
+++ bayom_e/model_handling/model_components.py
@@ -4,15 +4,14 @@ import pyomo.environ as pyo
 def Available_Acres_Constraint(model) -> pyo.ConcreteModel:
     """ BMPs within a BMPGRP cannot use more than the acres in a LRSEG,LOADSRC """
 
-    def additive_bmps_acre_bound_rule(model, gamma, l, u):
-        temp = sum([model.x[b, l, u]
-                    if (((b, gamma) in model.BMPGRPING) & ((b, u) in model.BMPSRCLINKS))
+    def additive_bmps_acre_bound_rule(mdl, gamma, l, u, h):
+        temp = sum([mdl.x[b, l, u, h]
+                    if (((b, gamma) in mdl.BMPGRPING) & ((b, u) in mdl.BMPSRCLINKS))
                     else 0
-                    for b in model.BMPS])
-        return None, temp, model.alpha[l, u]
+                    for b in mdl.BMPS])
+        return None, temp, mdl.alpha[l, u, h]
 
     model.Available_Acres_Constraint = pyo.Constraint(model.BMPGRPS,
-                                                      model.LRSEGS,
-                                                      model.LOADSRCS,
+                                                      model.PARCELS,
                                                       rule=additive_bmps_acre_bound_rule)
     return model
