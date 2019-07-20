5a46ef20641382925712ef05e91f4af5b0584ece
dkauf42 <dkauf42@gmail.com>
2019-07-20 19:25:01 -0400

    remove happy print statement

--- castjeeves/tests/conftest.py
+++ castjeeves/tests/conftest.py
@@ -5,5 +5,4 @@ from castjeeves.jeeves import Jeeves
 @pytest.fixture(scope='session')
 def source_resource(request):
     # Load the Source Data and Base Condition tables
-    print('in source-resource!')
     return Jeeves.loadInSourceDataFromSQL()
