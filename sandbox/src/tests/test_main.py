70e6c228ba94c193eb02f8886cc6e93f144ac4f4
dkauf42 <dkauf42@gmail.com>
2019-07-20 19:02:36 -0400

    skip all sandbox tests, since the sandbox package is out of date

--- sandbox/src/tests/test_main.py
+++ sandbox/src/tests/test_main.py
@@ -1,21 +1,28 @@
-import unittest
-
-from sandbox.src.sandboxer import main
+import pytest
 
 
-class TddForMain(unittest.TestCase):
+@pytest.mark.skip('sandbox is out-of-date, and not currently usable', allow_module_level=True)
+def resource_a(request):
+    from sandbox.src.sandboxer import main
+    return main
 
-    def setUp(self):
-        pass
 
-    def test_main_testcase1_runs_to_end_with_valid_arguments(self):
-        self.assertTrue(main(numinstances=1, testcase=1).successful_creation_log)
+@pytest.mark.skip('sandbox is out-of-date, and not currently usable', allow_module_level=True)
+def test_main_testcase1_runs_to_end_with_valid_arguments(resource_a):
+    assert resource_a(numinstances=1, testcase=1).successful_creation_log is True
 
-    def test_main_testcase2_runs_to_end_with_valid_arguments(self):
-        self.assertTrue(main(numinstances=1, testcase=2).successful_creation_log)
 
-    def test_main_testcase3_runs_to_end_with_valid_arguments(self):
-        self.assertTrue(main(numinstances=1, testcase=3).successful_creation_log)
+@pytest.mark.skip('sandbox is out-of-date, and not currently usable', allow_module_level=True)
+def test_main_testcase2_runs_to_end_with_valid_arguments(resource_a):
+    assert resource_a(numinstances=1, testcase=2).successful_creation_log is True
 
-    def test_main_raises_error_with_invalid_argument_values(self):
-        self.assertRaises(ValueError, main, numinstances=1, testcase=5)
+
+@pytest.mark.skip('sandbox is out-of-date, and not currently usable', allow_module_level=True)
+def test_main_testcase3_runs_to_end_with_valid_arguments(resource_a):
+    assert resource_a(numinstances=1, testcase=3).successful_creation_log is True
+
+
+@pytest.mark.skip('sandbox is out-of-date, and not currently usable', allow_module_level=True)
+def test_main_raises_error_with_invalid_argument_values(resource_a):
+    with pytest.raises(ValueError):
+        resource_a(numinstances=1, testcase=5)
--- sandbox/src/util/decisionspace/spaces/tests/test_animal.py
+++ sandbox/src/util/decisionspace/spaces/tests/test_animal.py
@@ -1,14 +1,15 @@
 import pytest
 
-from sandbox.src.util.decisionspace import DecisionSpace
-from sandbox.src.util.decisionspace.spaces.animal import Animal
 
 @pytest.fixture(scope='module')
 def resource_a(request):
+    from sandbox.src.util.decisionspace import DecisionSpace
+    from sandbox.src.util.decisionspace.spaces.animal import Animal
     # Load the Source Data and Base Condition tables
     jeeves = DecisionSpace.load_queries()
     return Animal(jeeves=jeeves)
 
 
+@pytest.mark.skip('sandbox is out-of-date, and not currently usable', allow_module_level=True)
 def test_something(resource_a):
         pass
