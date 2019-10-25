14f4f6b2cd2b31dd2b7c491f8bd39a80726e2bd9
dkauf42 <dkauf42@gmail.com>
2019-10-25 10:39:44 -0400

    add wait to setup.py test command

--- bin/docker_run_scripts/local_docker_check.py
+++ bin/docker_run_scripts/local_docker_check.py
@@ -13,7 +13,12 @@ def main():
     get_workspace_from_s3(log_level='INFO', s3_workspace_dir=s3_workspace_dir)
 
     # Run the tests
-    subprocess.Popen([f"python setup.py test"], shell=True)
+    p1 = subprocess.Popen([f"python /root/bayota/setup.py test"], shell=True)
+    p1.wait()
+    # Get return code from process
+    return_code = p1.returncode
+    if p1.returncode != 0:
+        raise RuntimeError(f"setup.py tests finished with non-zero code <{return_code}>")
 
 
 if __name__ == '__main__':
