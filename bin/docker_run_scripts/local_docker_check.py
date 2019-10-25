066eb7333f5e4e84050575bd18cb82e10c7153ff
dkauf42 <dkauf42@gmail.com>
2019-10-25 10:33:14 -0400

    redo the local_docker_check so that it just runs the pytests

--- bin/docker_run_scripts/local_docker_check.py
+++ bin/docker_run_scripts/local_docker_check.py
@@ -1,31 +1,20 @@
-import os
-import docker
+""" Run the setup.py tests for BayOTA
 
-from bayota_settings.base import get_bayota_version, get_model_instances_dir, \
-    get_scripts_dir, get_spec_files_dir, get_control_dir, get_model_specs_dir
+"""
+import sys
+import subprocess
+from bayota_util.s3_operations import get_workspace_from_s3
+from bayota_settings.base import get_bayota_version
 
-docker_client = docker.from_env()
 
-# Paths are specified and mapped to 'incontainer' directories
-docker_image = 'bayota_conda_app'
+def main():
+    # Pull the workspace from s3 into the container
+    s3_workspace_dir = 'optimization/ws_copies/bayota_ws_' + get_bayota_version()
+    get_workspace_from_s3(log_level='INFO', s3_workspace_dir=s3_workspace_dir)
 
-incontainer_workspace = '/root/bayota_ws'
-incontainer_control_dir = incontainer_workspace + '/control'
-incontainer_specfiles_dir = incontainer_workspace + '/specification_files'
-incontainer_modelinstances_dir = incontainer_workspace + '/temp/specification_files'
+    # Run the tests
+    subprocess.Popen([f"python setup.py test"], shell=True)
 
-volumes_dict = {f"{get_control_dir()}": {'bind': f"{incontainer_control_dir}", 'mode': 'rw'},
-                f"{get_spec_files_dir()}": {'bind': f"{incontainer_specfiles_dir}", 'mode': 'rw'},
-                f"{get_model_instances_dir()}": {'bind': f"{incontainer_modelinstances_dir}", 'mode': 'rw'}}
 
-print(volumes_dict)
-print(type(volumes_dict))
-
-# Job command is composed.
-model_generator_script = '/root/bayota/bin/slurm_scripts/run_step2_generatemodel.py'
-study_control_file = os.path.join(incontainer_control_dir, 'step1_studycon65929049-0ceb-46aa-a002-9bb8804175ae.yaml')
-CMD = f"{model_generator_script} -cf {study_control_file}"
-print(f'Job command is: "{CMD}"')
-
-# Job command is run in docker image.
-response = docker_client.containers.run('bayota_conda3_app', CMD, volumes=volumes_dict)
+if __name__ == '__main__':
+    sys.exit(main())
