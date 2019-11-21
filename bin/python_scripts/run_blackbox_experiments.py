49df97b4103197d129fc277616eedf43a59925d5
dkauf42 <dkauf42@gmail.com>
2019-11-21 12:53:49 -0500

    use pygmo in blackbox experiment script

--- bin/python_scripts/run_blackbox_experiments.py
+++ bin/python_scripts/run_blackbox_experiments.py
@@ -5,29 +5,71 @@ Example usage command:
     ./bin/scripts_by_level/run_single_study.py --dryrun -f /Users/Danny/Desktop/CATEGORIES/CAREER_MANAGEMENT/CRC_ResearchScientist_Optimization/Optimization_Tool/2_ExperimentFolder/bayota/bin/study_specs/single_study_specs/adamsPA_0001.yaml
 """
 
-import os
+import math
+import yaml
+import numpy as np
+import pandas as pd
+from itertools import product
 
-from bayom_e.model_handling import load_model_pickle
-from bin.slurm_scripts import run_step2_generatemodel
+import pyomo.environ as pyo
 
-from bayota_settings.base import get_output_dir, get_model_instances_dir
-from bayota_settings.log_setup import root_logger_setup
+from bayota_settings.base import get_bayota_version
+print("using bayota version '%s'" % get_bayota_version())
 
-logger = root_logger_setup()
+from bayota_settings.base import get_source_csvs_dir
+print("getting source csvs from: '%s'" % get_source_csvs_dir())
 
-outdir = get_output_dir()
+from bayota_util.spec_and_control_handler import read_spec
+from bayom_e.data_handling.data_interface import get_dataplate
+from bayom_e.model_handling.interface import check_for_problems_in_data_before_model_construction
 
+from bayom_e.model_handling.builders.nonlinear import NonlinearVariant
+from bayom_e.model_handling.builders.linear import LinearVariant
 
-def main(dryrun=False):
-    logprefix = '** Blackbox Experiment **: '
-    saved_model_file = os.path.join(get_model_instances_dir(),
-                                           'modelinstance_costmin_total_percentreduction_CalvertMD.pickle')
+from bayom_e.model_handling.builders.modelbuilder import ModelBuilder
 
-    run_step2_generatemodel.main(geography_name='CalvertMD', model_spec_name='costmin_total_percentreduction',
-                                 saved_model_name=saved_model_file, dryrun=False,
-                                 baseloadingfilename='2010NoActionLoads.csv')
+from bayom_e.model_handling.interface import build_model
 
-    mdlhandler = load_model_pickle(savepath=saved_model_file, dryrun=dryrun, logprefix=logprefix)
+from bayom_e.model_handling.utils import model_as_func_for_pygmo
 
-    mdlhandler.model
+import pygmo as pg
 
+# modelspec = '/Users/Danny/bayota_ws_0.1b2/specification_files/model_specs/costmin_total_Npercentreduction_updated_percentreduction.yaml'
+model_spec_name = 'costmin_total_Npercentreduction_updated_percentreduction'
+geoscale = 'county'
+geoentities = 'Adams, PA'
+baseloadingfilename = '2010NoActionLoads_updated.csv'
+
+# with open(modelspec, 'r') as stream:
+#     modelspec_dict = yaml.safe_load(stream)
+# modelspec_dict['variant'] = 'lp'
+# display(modelspec_dict)
+
+my_model, dp = build_model(model_spec_name, geoscale, geoentities, baseloadingfilename,
+                           savedata2file=False, log_level='INFO')
+
+prob_dim = len(my_model.x)
+print(f"--DIAGNOSTIC--\nprob_dim={prob_dim}")
+
+my_prob = model_as_func_for_pygmo(prob_dim, pyomo_model=my_model,
+                                  objective1_name='total_cost_expr', objective1_indexer=None, objective1_sign=1,
+                                  objective2_name='percent_reduction_expr', objective2_indexer='N', objective2_sign=-1)
+
+# create UDP
+prob = pg.problem(my_prob)
+
+# create population
+pop = pg.population(prob, size=20)
+
+# select algorithm
+algo = pg.algorithm(pg.nsga2(gen=40))
+
+# run optimization
+pop = algo.evolve(pop)
+
+# extract results
+fits, vectors = pop.get_f(), pop.get_x()
+print(f"--DIAGNOSTIC--\nfits={fits}")
+
+# extract and print non-dominated fronts
+ndf, dl, dc, ndr = pg.fast_non_dominated_sorting(fits)
